34c56a5f77b973e6b0cf5cea6396b198d2c10a7c
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-09-02 17:22:36 +0200

    Gathering pieces in app component to make a new calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,55 @@
 import React from 'react';
-import './App.css';
+import styled from 'styled-components';
+import {useQuery} from 'react-query';
+
+import {PageHeader, TestResults, TestsFilter, TestsSummary} from '@organisms';
+import {getAllTests} from '@services/Tests';
+
+const MainTableStyles = styled.table`
+  position: relative;
+  top: 0;
+  left: var(--font-size-6xl);
+  width: 90%;
+  border: 1px solid var(--color-gray-secondary);
+  border-top-style: hidden;
+`;
+
+const StyledTestResults = styled.tr`
+  border-left-style: hidden;
+  border-bottom-style: 1px solid var(--color-gray-secondary);
+`;
+
+const StyledTestFilter = styled.tr`
+  border-right-style: hidden;
+  border-left-style: hidden;
+`;
+
+const StyledTestSummary = styled.tr`
+  border-right-style: hidden;
+  height: 100%;
+`;
 
 function App() {
+  const {data, error, isFetching} = useQuery('tests', getAllTests);
+
   return (
-    <div className="App">
-      Kubtest Dashboard
-    </div>
+    <>
+      {error && 'Something went wrong'}
+      {isFetching && 'Loading...'}
+      {console.log(data)}
+      <PageHeader />
+      <MainTableStyles>
+        <StyledTestResults>
+          <TestResults />
+        </StyledTestResults>
+        <StyledTestFilter>
+          <TestsFilter />
+        </StyledTestFilter>
+        <StyledTestSummary>
+          <TestsSummary />
+        </StyledTestSummary>
+      </MainTableStyles>
+    </>
   );
 }
 
