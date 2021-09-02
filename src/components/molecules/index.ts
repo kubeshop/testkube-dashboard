eb7f866187c765ac981c0c0d558ab35aab30d104
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-09-02 17:17:12 +0200

    Export TestsList and TestDescription from the molecules

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -2,3 +2,5 @@ export {default as DocLinks} from './DocLinks/DocLinks';
 export {default as PageTitle} from './PageTitle/Pagetitle';
 export {default as ResultDatePicker} from './ResultsDatePicker/ResultDatePicker';
 export {default as TestStatus} from './TestStatus/TestStatus';
+export {default as TestsList} from './TestsList/TestsList';
+export {default as TestDescription} from './TestDescription/TestDescription';
