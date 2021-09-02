c3d08cf3deb9d5740be7cd30d8b0447ad08820cf
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-09-02 17:21:47 +0200

    Add queryCAche parameters to allow to refresh calls every 20 seconds

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { Provider } from 'react-redux';
-import { store } from '@redux/store';
+import {QueryClient, QueryClientProvider} from 'react-query';
 import App from './App';
 import './styles/variables.css';
 import 'antd/dist/antd.css';
 import GlobalStyle from './styles/globalStyles';
 
+const queryCache = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+      staleTime: 20000,
+    },
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
+    <QueryClientProvider client={queryCache}>
       <GlobalStyle />
       <App />
-    </Provider>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
