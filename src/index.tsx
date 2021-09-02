e1008676a3e7dca18f96b3a9a31086836141dc6d
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-09-02 17:07:22 +0200

    Import and inject custom styles and variables into root level of project

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,13 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { store } from '@redux/store';
 import App from './App';
+import './styles/variables.css';
+import 'antd/dist/antd.css';
+import GlobalStyle from './styles/globalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+      <GlobalStyle />
       <App />
     </Provider>
   </React.StrictMode>,
