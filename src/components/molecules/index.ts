d4e4c7f549e32ba14b36043172bbcf069bdef037
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-09-02 17:13:43 +0200

    Export ResultDatePicker and TestStatus components

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,2 +1,4 @@
 export {default as DocLinks} from './DocLinks/DocLinks';
 export {default as PageTitle} from './PageTitle/Pagetitle';
+export {default as ResultDatePicker} from './ResultsDatePicker/ResultDatePicker';
+export {default as TestStatus} from './TestStatus/TestStatus';
