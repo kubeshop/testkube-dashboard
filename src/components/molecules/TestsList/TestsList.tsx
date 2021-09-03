4839340e3c4725f14f230ce6057ff8a943ca2d5c
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-09-03 12:49:03 +0200

    Add image functionality for rendering different type of svg

--- src/components/molecules/TestsList/TestsList.tsx
+++ src/components/molecules/TestsList/TestsList.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 
-import {Typography} from '@atoms';
+import {Typography, Image} from '@atoms';
+import RunningTestIcon from '@assets/testRunningIcon.svg';
+import FailedTestIcon from '@assets/testFailedIcon.svg';
+import SuccessTestIcon from '@assets/testSuccessIcon.svg';
+
+interface ITestTypes {
+  testStatus: 'running' | 'failed' | 'success';
+}
 
 const ListItem = styled.div`
   display: flex;
   overflow: hidden;
+  align-items: flex-start;
   transition: background 0.3s ease-in-out, color 0.3s ease-in-out;
 
   &:hover {
@@ -21,20 +29,45 @@ const TestListInformation = styled.div`
   margin: 10px;
 `;
 
+function RenderTestStatusSvgIcon({testStatus}: ITestTypes) {
+  return (
+    <Image
+      src={
+        testStatus === 'running'
+          ? RunningTestIcon
+          : testStatus === 'failed'
+          ? FailedTestIcon
+          : testStatus === 'success'
+          ? SuccessTestIcon
+          : ''
+      }
+      alt="testStatus"
+      type="svg"
+      width={20}
+      height={20}
+    />
+  );
+}
+
 const TestsList = () => {
+  const [testName, setTestName] = useState<string>('Test Name');
+  const [testType, setTestType] = useState<string>('Test Type');
+  const [testTimeStamp, setTestTimeStamp] = useState<string>('Test TimeStamp');
+  const [testStatus, setTestStatus] = useState<ITestTypes>({testStatus: 'success'});
+
   return (
     <ListItem>
       <TestListInformation>
-        <Typography variant="secondary">Test Name</Typography>
+        <Typography variant="secondary">{testName}</Typography>
       </TestListInformation>
       <TestListInformation>
-        <Typography variant="secondary">Test Type</Typography>
+        <Typography variant="secondary">{testType}</Typography>
       </TestListInformation>
       <TestListInformation>
-        <Typography variant="secondary">Test TimeStamp</Typography>
+        <Typography variant="secondary">{testTimeStamp}</Typography>
       </TestListInformation>
       <TestListInformation>
-        <Typography variant="secondary">Test Status</Typography>
+        <RenderTestStatusSvgIcon testStatus={testStatus.testStatus} />
       </TestListInformation>
     </ListItem>
   );
