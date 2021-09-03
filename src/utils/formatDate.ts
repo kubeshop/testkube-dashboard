8241d0547cdbba8e69fdd043160bf4f272212567
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-09-03 16:06:17 +0200

    Create timeStamptoDate function to convert date into desired UI

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,10 @@
+export const timeStampToDate = (timeStamp: string) => {
+  const date = new Date(timeStamp);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+  return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+};
