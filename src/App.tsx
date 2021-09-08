68f74127467898fd1f0ad0529bc0e34a9d671a17
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-09-08 15:09:05 +0200

    Add common functionality for the test context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import {useQuery} from 'react-query';
 
 import {PageHeader, TestResults, TestsFilter, TestsSummary} from '@organisms';
 import {getAllTests} from '@services/Tests';
 import {TestsContext} from '@context/testsContext';
+
+// import {Tests} from '@types';
+import {getDate} from '@utils/formatDate';
 
 const MainTableStyles = styled.table`
   position: relative;
@@ -31,17 +34,54 @@ const StyledTestSummary = styled.tr`
 `;
 
 function App() {
-  const {data, error, isFetching} = useQuery('tests', getAllTests);
+  const [selectedTestTypes, setSelectedTestTypes] = useState<string>('');
+  const [selectedTest, setSelectedTest] = useState<number | undefined>();
+  const [datas, setDatas] = useState([]);
+  const [selectedTimeIntervalTests, setSelectedTimeIntervalTests] = useState('');
+  const [latestDateTests, setLatestDateTests] = useState();
+  const {data, error, isFetching} = useQuery('tests', getAllTests, {refetchInterval: 60000});
 
   const tests = {
     data,
+    selectedTest,
+    setSelectedTest,
+    selectedTestTypes,
+    setSelectedTestTypes,
+    datas,
+    setDatas,
+    selectedTimeIntervalTests,
+    setSelectedTimeIntervalTests,
+    latestDateTests,
+    setLatestDateTests,
   };
+
+  React.useEffect(() => {
+    if (data) {
+      const filteredTests =
+        selectedTestTypes === 'all'
+          ? data.ExecutionSummary
+          : data.ExecutionSummary.filter((test: any) => test.status === selectedTestTypes);
+
+      setDatas(filteredTests);
+    }
+  }, [selectedTestTypes]);
+
+  React.useEffect(() => {
+    const filteredTestsIntervals = data?.ExecutionSummary.filter(
+      (test: any) => getDate(test['start-time']) === getDate(selectedTimeIntervalTests)
+    );
+    setDatas(filteredTestsIntervals);
+  }, [selectedTimeIntervalTests]);
+
+  React.useEffect(() => {
+    // connect
+  }, [latestDateTests]);
 
   return (
     <>
       {error && 'Something went wrong'}
       {isFetching && 'Loading...'}
-      <TestsContext.Provider value={tests.data}>
+      <TestsContext.Provider value={tests}>
         <PageHeader />
         <MainTableStyles>
           <StyledTestResults>
