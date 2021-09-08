32ccb7c6926a8ec1c8fc47da900b166c4767129b
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-09-08 15:08:35 +0200

    Update test descriptions to display desired data

--- src/components/molecules/TestDescription/TestDescription.tsx
+++ src/components/molecules/TestDescription/TestDescription.tsx
@@ -1,41 +1,113 @@
-import React from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import styled from 'styled-components';
 
-// import {Typography} from '@atoms';
+import {TestsContext} from '@context/testsContext';
+import {timeStampToDate, getDuration} from '@utils/formatDate';
+import {RenderTestStatusSvgIcon, Typography} from '@atoms';
 
-const StyledTestDescriptionContainer = styled.div`
+const StyledTestDescriptionContainer = styled.section`
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
 `;
 
-// const StyledTestImage = styled.div`
-//   width: 100%;
-//   height: 100%;
-// `;
-
-const StyledTestName = styled.div`
+const StyledTestDescriptionIcon = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  position: relative;
+  top: var(--space-md);
+  left: var(--space-md);
+`;
+
+const StyledTestStatus = styled.div`
+  margin-left: var(--space-md);
+`;
+
+const StyledTestName = styled.div`
+  /* position: relative;
+  top: 20px;
+  left: 60px; */
+`;
+
+const StyledTestTimes = styled.div`
+  display: flex;
+
+  /* &:nth-child(1) {
+    margin-left: 20px;
+  } */
+`;
+
+const StyledEndedAt = styled.div``;
+
+const StyledDuration = styled.div`
+  margin-left: 20px;
 `;
 
 const StyledTestDescription = styled.div`
-  width: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: baseline;
+  justify-content: center;
+  position: relative;
+  top: 20px;
+  left: 85px;
 `;
 
 const TestDescription = () => {
+  const [testDescription, setTestDescription] = useState<any>({});
+  const tests: any = useContext(TestsContext);
+
+  useEffect(() => {
+    if (tests.selectedTest) {
+      const test = tests.data.ExecutionSummary.find((t: any) => tests.selectedTest === t['id']);
+      setTestDescription(test);
+    }
+  }, [tests.selectedTest]);
+
+  const renderTestStatus = (testStatus: string) => {
+    return testStatus === 'pending'
+      ? 'PENDING'
+      : testStatus === 'failed'
+      ? 'FAILED'
+      : testStatus === 'success'
+      ? 'SUCCESS'
+      : testStatus === 'queued'
+      ? 'QUEUED'
+      : '';
+  };
+
   return (
-    <StyledTestDescriptionContainer>
-      <StyledTestName>
-        {/* <StyledTestImage>*Image</StyledTestImage> */}
-        {/* <Typography variant="secondary">Test Name</Typography> */}
-      </StyledTestName>
-      <StyledTestDescription>
-        {/* <Typography variant="secondary">Test Description</Typography> */}
-      </StyledTestDescription>
-    </StyledTestDescriptionContainer>
+    <>
+      {tests.selectedTest && (
+        <StyledTestDescriptionContainer>
+          <StyledTestDescriptionIcon>
+            <RenderTestStatusSvgIcon testStatus={testDescription.status} width={50} height={50} />
+            <StyledTestStatus>
+              <Typography variant="secondary"> TEST {renderTestStatus(testDescription.status)}</Typography>
+            </StyledTestStatus>
+          </StyledTestDescriptionIcon>
+          <StyledTestDescription>
+            <StyledTestName>
+              <Typography variant="secondary"> Name</Typography>
+              <Typography variant="secondary">{testDescription['script-name']}</Typography>
+            </StyledTestName>
+
+            <StyledTestTimes>
+              <StyledEndedAt>
+                <Typography variant="secondary">Ended At</Typography>
+                <Typography variant="secondary">{timeStampToDate(testDescription['end-time'])}</Typography>
+              </StyledEndedAt>
+              <StyledDuration>
+                <Typography variant="secondary">Duration</Typography>
+                <Typography variant="secondary">{getDuration(testDescription['end-time'])}</Typography>
+              </StyledDuration>
+            </StyledTestTimes>
+          </StyledTestDescription>
+        </StyledTestDescriptionContainer>
+      )}
+    </>
   );
 };
 
