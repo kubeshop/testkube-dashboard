5af49dca0aa95fc497b5afd2ffcea5c25abb60ae
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-09-08 15:04:19 +0200

    Add duration and date and getlatest time functions

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -6,5 +6,25 @@ export const timeStampToDate = (timeStamp: string) => {
   const hour = date.getHours();
   const minute = date.getMinutes();
   const second = date.getSeconds();
-  return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+  return `${day}/${month}/${year} ${hour}:${minute}:${second}`;
+};
+
+export const getDuration = (timeStamp: string) => {
+  const date = new Date(timeStamp);
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+  return `${hour}:${minute}:${second}`;
+};
+
+export const getDate = (timeStamp: string) => {
+  const date = new Date(timeStamp);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  return `${year}-${month}-${day}`;
+};
+
+export const getLatestDate = (timeStamp: any) => {
+  return new Date(Math.max(...timeStamp.map((t: any) => new Date(t['start-time']))));
 };
