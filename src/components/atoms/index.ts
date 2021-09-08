4fe08e79c8f667f8db8a19a4aeee290a1463a600
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-09-08 15:05:56 +0200

    Refactor Render test icon

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,3 +1,4 @@
 export { default as Typography } from './Typography/Typography';
 export { default as Button } from './Button/Button';
 export { default as Image } from './Image/Image';
+export { default as RenderTestStatusSvgIcon } from './RenderIcons/RenderIcons';
