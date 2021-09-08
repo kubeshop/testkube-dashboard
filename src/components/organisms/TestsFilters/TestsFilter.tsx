12f4177d389b42f90881f3f7b8563ad623b3cc13
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-09-08 15:07:19 +0200

    Apply test status as filters

--- src/components/organisms/TestsFilters/TestsFilter.tsx
+++ src/components/organisms/TestsFilters/TestsFilter.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 
 import {Typography, Button} from '@atoms';
 
+import {TestsContext} from '@context/testsContext';
+
 const StyleTestsFilterContainer = styled.div`
   display: flex;
   align-items: baseline;
@@ -16,15 +18,17 @@ const StyleTestFilterButtons = styled.div`
 `;
 
 const TestsFilter = () => {
+  const tests: any = React.useContext(TestsContext);
+
   return (
     <StyleTestsFilterContainer>
       <Typography variant="secondary">Tests</Typography>
       <StyleTestFilterButtons>
         <Typography variant="secondary">Show: </Typography>
-        <Button>All</Button>
-        <Button>Running</Button>
-        <Button>Passed</Button>
-        <Button>Failed</Button>
+        <Button onClick={() => tests.setSelectedTestTypes('all')}>All</Button>
+        <Button onClick={() => tests.setSelectedTestTypes('pending')}>Running</Button>
+        <Button onClick={() => tests.setSelectedTestTypes('success')}>Passed</Button>
+        <Button onClick={() => tests.setSelectedTestTypes('failed')}>Failed</Button>
       </StyleTestFilterButtons>
     </StyleTestsFilterContainer>
   );
