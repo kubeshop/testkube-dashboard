ed5afa655c158d2103a4dfaf71441ced2c96a3a1
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-09-09 04:24:47 +0200

    Remove redux from tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from '@redux/store';
+import {render} from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+  const {getByText} = render(<App />);
 
   expect(getByText(/learn/i)).toBeInTheDocument();
 });
