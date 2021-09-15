c515d2732010deaca6c3dca71e21a057486fc5e0
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-09-15 20:47:38 +0200

    Add colors to globalStyles and media queries

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle`
     body {
       margin: 0;
       padding: 0;
@@ -40,7 +40,28 @@ const GlobalStyle = createGlobalStyle`
     .ant-picker-content > thead > tr > th {
       color: var(--color-light-primary);
     }
-
   `;
 
-export default GlobalStyle;
+/* Media Sizes */
+const size = {
+  mobileS: '320px',
+  mobileM: '375px',
+  mobileL: '425px',
+  tablet: '768px',
+  laptop: '1024px',
+  laptopL: '1440px',
+  desktop: '2560px'
+};
+
+/* Devices */
+
+export const device = {
+  mobileS: `(min-width: ${size.mobileS})`,
+  mobileM: `(min-width: ${size.mobileM})`,
+  mobileL: `(min-width: ${size.mobileL})`,
+  tablet: `(min-width: ${size.tablet})`,
+  laptop: `(min-width: ${size.laptop})`,
+  laptopL: `(min-width: ${size.laptopL})`,
+  desktop: `(min-width: ${size.desktop})`,
+  desktopL: `(min-width: ${size.desktop})`
+};
