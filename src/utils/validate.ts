03b58bbee60b7b67574e07ad83eed837098dbecc
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-09-15 20:49:40 +0200

    Add url validation methods against user inputs

---
+++ src/utils/validate.ts
@@ -0,0 +1,17 @@
+export const validateUrl = (url: string): boolean => {
+  const pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+    "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
+    "((\\d{1,3}\\.){3}\\d{1,3}))" +
+    "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
+    "(\\?[;&a-z\\d%_.~+=-]*)?" +
+    "(\\#[-a-z\\d_]*)?$",
+    "i"
+  );
+  return pattern.test(url);
+};
+
+export const getQueryStringFromUrl = (url: string): string => {
+  const queryString = url.split("?")[1];
+  return queryString;
+};
