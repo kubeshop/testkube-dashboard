6ab2603bd61c16f96cd27d8bdce06a91f436b533
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-09-21 16:46:34 +0200

    Remove unneccessary wrappers for test status

--- src/components/molecules/TestStatus/TestStatus.tsx
+++ src/components/molecules/TestStatus/TestStatus.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import {Typography} from '@atoms';
 
@@ -8,24 +7,15 @@ interface ITestStatusProps {
   totalTests: string;
 }
 
-const StyledTestTitleResult = styled.div`
-  position: relative;
-  left: 50px;
-`;
-
 const TestStatus: React.FC<ITestStatusProps> = ({testTitle, totalTests}) => {
   return (
     <>
-      <StyledTestTitleResult>
-        <Typography variant="secondary" color="tertiary">
-          {testTitle}
-        </Typography>
-      </StyledTestTitleResult>
-      <StyledTestTitleResult>
-        <Typography variant="quinary" color="quaternary">
-          {totalTests}
-        </Typography>
-      </StyledTestTitleResult>
+      <Typography variant="secondary" color="tertiary">
+        {testTitle}
+      </Typography>
+      <Typography variant="quinary" color="quaternary">
+        {totalTests}
+      </Typography>
     </>
   );
 };
