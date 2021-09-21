08ff29b2fd4720b8eccaaa9b49a37b57fccb78e3
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-09-21 16:47:16 +0200

    Adjust test description component

--- src/components/molecules/TestDescription/TestDescription.tsx
+++ src/components/molecules/TestDescription/TestDescription.tsx
@@ -1,25 +1,13 @@
-import React, {useContext, useState, useEffect} from 'react';
+import React, {useContext, useState} from 'react';
 import styled from 'styled-components';
 import {useQuery} from 'react-query';
-// import * as queryString from 'query-string';
 
 import {TestsContext} from '@context/testsContext';
 import {timeStampToDate, getDuration} from '@utils/formatDate';
 import {RenderTestStatusSvgIcon, Typography} from '@atoms';
-// import {getQueryStringFromUrl} from '@utils/validate';
-
-const StyledTestDescriptionContainer = styled.section`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  top: var(--space-md);
-  left: var(--space-md);
-`;
 
 const StyledTestDescriptionIcon = styled.div`
   position: relative;
-  top: -135px;
 `;
 
 const StyledTestDescription = styled.div`
@@ -30,23 +18,10 @@ const StyledTestDescription = styled.div`
 `;
 
 const TestDescription = () => {
-  const [testDescription, setTestDescription] = useState<any>({});
   const [api, setApi] = useState<string>(localStorage.getItem('apiEndpoint') || '');
   const tests: any = useContext(TestsContext);
 
   const {data, error} = useQuery(['test', tests.selectedTest], () => {
-    // const url = getQueryStringFromUrl(window.location.href);
-    // console.log('URL', url);
-
-    // const params = new URL(window.location.href).searchParams;
-    // console.log('PARAMS', params.get('apiEndpoint'));
-
-    // localStorage.getItem('apiEndpoint');
-    // const parsed = queryString.parse(window.location.search);
-    // console.log(parsed);
-
-    // console.log('PARAMS', params);
-
     if (api) {
       return fetch(`${api}/${tests.selectedTest}`).then(res => res.json());
     }
@@ -58,13 +33,6 @@ const TestDescription = () => {
       setApi(apiFromUser);
     }
   }, []);
-
-  useEffect(() => {
-    if (tests.selectedTest) {
-      const test = tests.data.find((t: any) => tests.selectedTest === t['id']);
-      setTestDescription(test);
-    }
-  }, [tests.selectedTest]);
 
   const renderTestStatus = (testStatus: string) => {
     return testStatus === 'pending'
@@ -80,24 +48,20 @@ const TestDescription = () => {
 
   return (
     <>
-      {/* {console.log('script name', data['script-name'])} */}
-      {/* {console.log('Ended At', data.execution['end-time'])}
-      {console.log('Duration', data.execution['script-name'])}
-      {console.log('script type', data['script-type'])} */}
-      {tests.selectedTest && (
-        <StyledTestDescriptionContainer>
+      {error && <Typography variant="secondary">Something went wrong...</Typography>}
+      {tests.selectedTest && data && (
+        <>
           <StyledTestDescriptionIcon>
-            <RenderTestStatusSvgIcon testStatus={testDescription.status} width={50} height={50} />
+            <RenderTestStatusSvgIcon testStatus={data.execution.status} width={50} height={50} />
           </StyledTestDescriptionIcon>
           <StyledTestDescription>
-            <Typography variant="secondary"> TEST {renderTestStatus(testDescription.status)}</Typography>
+            <Typography variant="secondary"> TEST {renderTestStatus(data.execution.status)}</Typography>
             <div>
               <Typography variant="secondary" font="bold">
                 Name
               </Typography>
               <Typography variant="secondary" style={{marginTop: '-15px'}}>
-                {/* {testDescription['script-name']} */}
-                {testDescription['script-name'] && testDescription['script-name']}
+                {data['script-name']}
               </Typography>
             </div>
             <div>
@@ -105,7 +69,7 @@ const TestDescription = () => {
                 Ended At
               </Typography>
               <Typography variant="secondary" style={{marginTop: '-15px'}}>
-                {testDescription['end-time'] ? timeStampToDate(testDescription['end-time']) : '-'}
+                {data.execution['end-time'] ? timeStampToDate(data.execution['end-time']) : '-'}
               </Typography>
             </div>
             <div>
@@ -113,8 +77,8 @@ const TestDescription = () => {
                 Duration
               </Typography>
               <Typography variant="secondary" style={{marginTop: '-15px'}}>
-                {testDescription['end-time']
-                  ? getDuration(testDescription['start-time'], testDescription['end-time'])
+                {data.execution['end-time']
+                  ? getDuration(data.execution['start-time'], data.execution['end-time'])
                   : '-'}
               </Typography>
             </div>
@@ -123,11 +87,11 @@ const TestDescription = () => {
                 Type
               </Typography>
               <Typography variant="secondary" style={{marginTop: '-15px'}}>
-                {testDescription['script-type']}
+                {data['script-type']}
               </Typography>
             </div>
           </StyledTestDescription>
-        </StyledTestDescriptionContainer>
+        </>
       )}
     </>
   );
