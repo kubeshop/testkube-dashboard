c75cf75c4433f87e1fb0a4eaa037eebb182369f1
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-09-22 14:30:56 +0200

    Validate the url with the matched host protocol

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,6 +1,6 @@
 export const validateUrl = (url: string): boolean => {
   const pattern = new RegExp(
-   "^(https?:\\/\\/)?" + // protocol
+    "^(https?:\\/\\/)?" +
     "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
     "((\\d{1,3}\\.){3}\\d{1,3}))" +
     "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
@@ -15,4 +15,21 @@ export const getQueryStringFromUrl = (url: string) => {
 
   const params = new URL(url).searchParams;
   return params.get('apiEndpoint');
+};
+
+export const matchUrlProtocol = (url: string) => {
+  const hostProtocol = window.location.protocol;
+  if (!url) {
+    alert("Invalid URL, You are trying to manipulate the url, please provide a correct url endpoint");
+  }
+
+  const apiEndpointProtocol = new URL(url).protocol;
+
+  if (hostProtocol !== apiEndpointProtocol) {
+    const matchedUrlProtocol = url.replace(apiEndpointProtocol, hostProtocol);
+    localStorage.setItem('apiEndpoint', matchedUrlProtocol);
+    return;
+  }
+
+  localStorage.setItem('apiEndpoint', url);
 };
