ecdb24cac2b6508b37cac4b0a74af16df20366ec
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-10-01 13:53:04 +0200

    Refactor fetch tests queries into seperate hooks

---
+++ src/hooks/tests.ts
@@ -0,0 +1,40 @@
+import React, { useContext, useState } from 'react';
+import { useQuery } from 'react-query';
+
+import { config } from '@constants/config';
+import {TestsContext} from '@context/testsContext';
+
+export const useFetchTest = () => {
+  const [api, setApi] = useState<string>(localStorage.getItem(config.apiEndpoint) || '');
+    const tests: any = useContext(TestsContext);
+  const {data, error} = useQuery(['test', tests.selectedTest], () => {
+    if (api) {
+      return fetch(`${api}/${tests.selectedTest}`).then(res => res.json());
+    }
+  });
+
+  React.useEffect(() => {
+    const apiFromUser = localStorage.getItem(config.apiEndpoint);
+    if (apiFromUser) {
+      setApi(apiFromUser);
+    }
+  }, []);
+
+  return { data, error };
+};
+
+export const useFetchTests = () => {
+
+  const { data, error } = useQuery(
+    'tests',
+    () => {
+      const url = localStorage.getItem(config.apiEndpoint);
+      if (url) {
+        return fetch(url).then(res => res.json());
+      }
+    },
+    { refetchInterval: 5000 }
+  );
+
+  return {data, error};
+};
