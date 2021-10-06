a96714b918a9acd0457fa343397c3f703afeb35c
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-10-06 14:00:27 +0200

    Update test status renders

--- src/components/molecules/TestStatus/TestStatus.tsx
+++ src/components/molecules/TestStatus/TestStatus.tsx
@@ -1,21 +1,43 @@
 import React from 'react';
-
-import {Typography} from '@atoms';
+import styled from 'styled-components';
 
 interface ITestStatusProps {
   testTitle: string;
   totalTests: string | undefined;
 }
 
-const TestStatus: React.FC<ITestStatusProps> = ({testTitle, totalTests}) => {
+interface ITestStatusStyles {
+  testTitleColor?: string;
+  totalTestsColor?: string;
+}
+
+const StyledTestStatus = styled.div`
+  text-align: start;
+`;
+
+const StyledTestTitle = styled.span<ITestStatusStyles>`
+  font-size: var(--font-size-md);
+  color: ${props => (props.testTitleColor === 'white' ? 'var(--color-light-primary)' : 'var(--color-yellow-primary)')};
+`;
+
+const StyledTotalTests = styled.span<ITestStatusStyles>`
+  font-size: var(--font-size-mid-4xl);
+  color: ${props => (props.totalTestsColor === 'white' ? 'var(--color-light-primary)' : 'var(--color-yellow-primary)')};
+`;
+
+const TestStatus: React.FC<ITestStatusProps & ITestStatusStyles> = ({
+  testTitle,
+  totalTests,
+  testTitleColor,
+  totalTestsColor,
+}) => {
   return (
     <>
-      <Typography variant="secondary" color="tertiary">
-        {testTitle}
-      </Typography>
-      <Typography variant="quinary" color="quaternary">
-        {totalTests || '0'}
-      </Typography>
+      <StyledTestStatus>
+        <StyledTestTitle testTitleColor={testTitleColor}> {testTitle}</StyledTestTitle>
+        <br />
+        <StyledTotalTests totalTestsColor={totalTestsColor}>{totalTests || '0'}</StyledTotalTests>
+      </StyledTestStatus>
     </>
   );
 };
