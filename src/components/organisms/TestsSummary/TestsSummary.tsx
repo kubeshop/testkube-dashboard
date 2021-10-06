36d60a76aedd0de5b2c612527b1397bd5a42a8b9
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-10-06 13:59:17 +0200

    Adjust test table styles width

--- src/components/organisms/TestsSummary/TestsSummary.tsx
+++ src/components/organisms/TestsSummary/TestsSummary.tsx
@@ -6,8 +6,8 @@ import {TestsList, TestDescription} from '@molecules';
 const StyledTestList = styled.td`
   height: 400px;
   max-height: 500px;
-  min-width: 40%;
-  max-width: 40%;
+  min-width: 55%;
+  max-width: 55%;
   overflow: hidden;
   overflow-y: scroll;
   border-right: 1px solid var(--color-gray-secondary);
@@ -18,8 +18,8 @@ const StyledTestDescription = styled.td`
   display: flex;
   border: hidden;
   flex-grow: 4;
-  min-width: 60%;
-  max-width: 60%;
+  min-width: 45%;
+  max-width: 45%;
   border-right-style: hidden;
 `;
 
