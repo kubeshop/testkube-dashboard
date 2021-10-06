123cad91227c7a1c718cb82810e56ad1e7e0fb54
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-10-06 21:55:10 +0200

    Update validating method to append the version to the url input value

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -2,20 +2,21 @@ import {config} from '@constants/config';
 
 export const validateUrl = (url: string): boolean => {
   const pattern = new RegExp(
-    "^(https?:\\/\\/)?" +
-    "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
-    "((\\d{1,3}\\.){3}\\d{1,3}))" +
-    "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
-    "(\\?[;&a-z\\d%_.~+=-]*)?" +
-    "(\\#[-a-z\\d_]*)?$",
-    "i"
+    '^(https?:\\/\\/)?' +
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3}))' +
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
   );
+
   return pattern.test(url);
 };
 
 export const getQueryStringFromUrl = (url: string) => {
-
   const params = new URL(url).searchParams;
+
   return params.get(config.apiEndpoint);
 };
 
@@ -29,8 +30,9 @@ export const removeSpaceFromString = (url: string) => {
 
 export const matchEndpointProtocolWithHostProtocol = (url: string) => {
   const hostProtocol = window.location.protocol;
+
   if (!url) {
-    alert("Invalid URL, You are trying to manipulate the url, please provide a correct url endpoint");
+    alert('Invalid URL, You are trying to manipulate the url, please provide a correct url endpoint');
   }
 
   if (!validateUrl(url)) {
@@ -40,43 +42,31 @@ export const matchEndpointProtocolWithHostProtocol = (url: string) => {
   const apiEndpointProtocol = new URL(url).protocol;
 
   if (!apiEndpointProtocol) {
-    const newApiEndpointWithProtocol  = `${hostProtocol}//${url}`;
+    const newApiEndpointWithProtocol = `${hostProtocol}//${url}`;
     const trimmedUrl = removeSpaceFromString(newApiEndpointWithProtocol);
     const cleanUrl = RemoveLastTrailingSlashFromString(trimmedUrl);
-     localStorage.setItem(config.apiEndpoint, cleanUrl);
+    const finalUrl = `${cleanUrl}${config.apiVersion}`;
+
+    localStorage.setItem(config.apiEndpoint, finalUrl);
   }
 
   if (hostProtocol !== apiEndpointProtocol) {
     const matchedUrlProtocol = url.replace(apiEndpointProtocol, hostProtocol);
-     const trimmedUrl = removeSpaceFromString(matchedUrlProtocol);
+    const trimmedUrl = removeSpaceFromString(matchedUrlProtocol);
     const cleanUrl = RemoveLastTrailingSlashFromString(trimmedUrl);
-     localStorage.setItem(config.apiEndpoint, cleanUrl);
+    const finalUrl = `${cleanUrl}${config.apiVersion}`;
+
+    localStorage.setItem(config.apiEndpoint, finalUrl);
     return;
   }
 
   localStorage.setItem(config.apiEndpoint, url);
 };
 
-export const removeDuplicatesInQueryString = (originLocation: string) => {
-
-  const queryString = getQueryStringFromUrl(originLocation);
-  if (!queryString) {
-    return;
-  }
-
-  const queryStringArray = queryString.split(`${config.apiEndpoint}=`);
-
-  const uniqueQueryStringArray = [...new Set(queryStringArray)];
-
-  const uniqueQueryString = uniqueQueryStringArray.join('&');
-
-  // window.location.replace(`${window.location.origin}/?apiEndpoint=${uniqueQueryString}`);
-
-};
-
 export const cleanStorageWhenApiEndpointQueryStringIsAbsent = () => {
   const url = window.location.href;
   const apiEndpointQueryString = getQueryStringFromUrl(url);
+
   if (!apiEndpointQueryString) {
     localStorage.removeItem(config.apiEndpoint);
   }
@@ -85,6 +75,7 @@ export const cleanStorageWhenApiEndpointQueryStringIsAbsent = () => {
 export const getApiEndpointOnPageLoad = () => {
   const url = window.location.href;
   const queryString = getQueryStringFromUrl(url);
+
   if (!queryString) {
     localStorage.removeItem(config.apiEndpoint);
     return;
@@ -108,8 +99,10 @@ export const isHostProtocolSecure = () => {
 export const CheckIfQueryParamsExistsInUrl = (queryParams: string) => {
   const urlParams = new URLSearchParams(window.location.search);
   const myParam = urlParams.get(queryParams);
+
   if (!myParam) {
     return;
   }
+
   return myParam;
 };
