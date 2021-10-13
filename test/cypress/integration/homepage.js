265affffc609762cf06dc4adb28b791c8f03dd6a
Jacek Wysocki <jacek.wysocki@gmail.com>
2021-10-13 14:23:21 +0200

    tests: testing delay

--- test/cypress/integration/homepage.js
+++ test/cypress/integration/homepage.js
@@ -5,8 +5,11 @@ describe('The Home Page', () => {
     cy.visit('https://dashboard.testkube.io') 
     cy.contains('Please provide the TestKube API endpoint for your installation, which will have been provided to you by the TestKube installer.')
 
+
     cy.get('#url').type('https://demo.testkube.io/results/v1/executions')
     cy.get("button[type='submit']").click()
+
+    cy.wait(2000)
 
     // check if there is some data on grid (need to be more sophisticated)
     cy.get("div[role='cell'").then(cells => {
