3e558713dc179e9aa00f0ed9c1e7913d37549842
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-10-13 12:26:21 +0200

    Update button and height styles

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const MainTableStyles = styled.table`
 
 const StyledTestResults = styled.tr`
   display: flex;
-  height: 152px;
+  height: 140px;
   border-top-style: none;
   border-bottom-style: 1px solid var(--color-gray-secondary);
   word-wrap: break-word;
@@ -40,6 +40,7 @@ const StyledTestFilter = styled.tr`
   justify-content: space-between;
   border-right-style: hidden;
   border-left-style: hidden;
+  height: 70px;
 `;
 
 const StyledTestSummary = styled.tr`
