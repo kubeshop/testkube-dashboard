2071992a6e1d40dfb61e702c5f2e325546a0b67a
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-10-14 10:22:47 +0200

    Update Regex

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,8 +1,13 @@
 import {config} from '@constants/config';
 
 export const validateUrl = (url: string): boolean => {
-  const pattern = new RegExp(
-    '(https?://)?(?:w{1,3}.)?[^s.]+(?:.[a-z]+)*(?::d+)?((?:/w+)|(?:-w+))*/?(?![^<]*(?:</w+>|/?>))',
+    const pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$',
     'i'
   );
 
