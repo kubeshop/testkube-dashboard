49311e208c7934b918453356bd55901c15eacdd5
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-10-29 06:39:50 +0200

    Adjust notification popup config

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,25 +1,18 @@
 import { Notyf } from 'notyf';
 import { escapeHtml } from '@utils';
 
-const notyfOptions = {
+export function showSmallError(e: any, pureText: boolean = false, position?: string): void {
+  const notyf = new Notyf();
+
+  const notyfOptions = {
     position: {
-        x: 'right',
+        x: position || 'right',
         y: 'top'
     },
     duration: 3000,
     dismissible: true
 };
 
-export function showSmallError(e: any, pureText: boolean = false): void {
-    const notyf = new Notyf();
-
     // @ts-ignore
-    notyf.error({ message: pureText ? escapeHtml(e.message ? e.message : String(e)) : e, ...notyfOptions });
-}
-
-export function showSmallSuccess(message: string) {
-    const notyf = new Notyf();
-
-    // @ts-ignore
-    notyf.success({ message: escapeHtml(message), ...notyfOptions });
+    notyf.error({ message: pureText ? escapeHtml(e.message ? e.message : String(e)) : e,  ...notyfOptions });
 }
