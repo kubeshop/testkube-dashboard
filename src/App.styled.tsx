695399ad2f1f0779464de4787c8f11f78c9ab3c7
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-10-29 06:38:53 +0200

    Refacror styled components for App comp

---
+++ src/App.styled.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const MainTableStyles = styled.table`
+  table-layout: fixed;
+  width: 90vw;
+  height: 90vh;
+  text-align: center;
+  margin: 0 auto;
+`;
+
+export const StyledTestResults = styled.tr`
+  display: flex;
+  height: 140px;
+  border-top-style: none;
+  border-bottom-style: 1px solid var(--color-gray-secondary);
+  word-wrap: break-word;
+`;
+
+export const StyledTestFilter = styled.tr`
+  display: flex;
+  align-items: baseline;
+  justify-content: space-between;
+  border-right-style: hidden;
+  border-left-style: hidden;
+  height: 70px;
+`;
+
+export const StyledTestSummary = styled.tr`
+  border-top-style: hidden;
+  height: 85vh;
+  display: flex;
+
+  @media only screen and (min-width: 2500px) {
+    height: 85vh;
+  }
+`;
