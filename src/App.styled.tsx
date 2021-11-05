1af5ba01ae7d2da635e06e1fe9d2d2209b1f3b19
hamzahsn <amdouni.hamza.hsn@gmail.com>
2021-11-05 15:23:14 +0100

    Test media queries

--- src/App.styled.tsx
+++ src/App.styled.tsx
@@ -27,10 +27,24 @@ export const StyledTestFilter = styled.tr`
 
 export const StyledTestSummary = styled.tr`
   border-top-style: hidden;
-  height: 55vh;
   display: flex;
+  height: 85vh;
 
-  // @media only screen and (min-width: 2500px) {
-  //   height: 85vh;
-  // }
+  // TESTING DIFFERENT SCREENS
+
+  /* @media only screen and (min-width: 425px) {
+    height: 50vh;
+  }
+  @media only screen and (min-width: 768px) {
+    height: 55vh;
+  }
+  @media only screen and (min-width: 1024px) {
+    height: 64vh;
+  }
+  @media only screen and (min-width: 1440px) {
+    height: 72vh;
+  }
+  @media only screen and (min-width: 2500px) {
+    height: 83vh;
+  } */
 `;
