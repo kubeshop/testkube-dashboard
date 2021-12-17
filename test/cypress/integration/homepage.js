05b5b093001925292f576b5ad93d103d666f7e5b
ediv <ediv@opinov8.com>
2021-12-17 12:26:28 +0200

    feat: adjusted tests

--- test/cypress/integration/homepage.js
+++ test/cypress/integration/homepage.js
@@ -1,21 +1,8 @@
-const {createVerify} = require('crypto');
-
 describe('The Home Page', () => {
-  it('check if there are some entries on grid', () => {
-    // cy.visit('https://demo.testkube.io');
-    // cy.wait(2000);
-    // check if there is some data on grid (need to be more sophisticated)
-    // cy.get("td[role='cell'").then(cells => {
-    //   const count = Cypress.$(cells).length;
-    //   expect(count).to.be.greaterThan(0);
-    // });
-  });
-
-  it('homepage asks for URI', () => {
-    cy.get("[aria-label='setting']").click();
-    cy.wait(500);
-    cy.contains('TestKube API endpoint');
-    cy.get('#url').type('https://demo.testkube.io/results');
-    cy.get("button[type='submit']").click();
+  it('check if the table is renderer there', () => {
+    cy.visit('https://demo.testkube.io/apiEndpoint?apiEndpoint=https://demo.testkube.io/results/v1');
+    cy.get('a[href="/dashboard/scripts"]').click({multiple: true, force: true});
+    cy.wait(2000);
+    cy.get('.ant-table-tbody');
   });
 });
