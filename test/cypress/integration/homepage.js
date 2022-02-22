cef1a0ee420368d64470561b5afce252493d4b8c
Jacek Wysocki <jacek.wysocki@gmail.com>
2022-02-22 10:52:11 +0100

    fix: tests fixed for demo dashboard (#252)
    * fix: tests fixed for demo dashboard
    
    * feat: added Edward as codeowner

--- test/cypress/integration/homepage.js
+++ test/cypress/integration/homepage.js
@@ -1,8 +1,24 @@
 describe('The Home Page', () => {
-  it('check if the table is renderer there', () => {
+  it('Go to dashboard', () => {
     cy.visit('https://demo.testkube.io/apiEndpoint?apiEndpoint=https://demo.testkube.io/results/v1');
-    cy.get('a[href="/dashboard/scripts"]').click({multiple: true, force: true});
-    cy.wait(2000);
-    cy.get('.ant-table-tbody');
+  });
+
+  it('Test suites should be shown as default view', () => {
+    cy.get('h1').should('have.text', 'Test Suites ');
+  });
+
+  it('Load test suite executions list', () => {
+    cy.get('a[href="/dashboard/test-suite-executions"]:first').click();
+    cy.get('h1').should('have.text', 'Test Suite Executions ');
+  });
+
+  it('Load tests list', () => {
+    cy.get('a[href="/dashboard/tests"]:first').click();
+    cy.get('h1').should('have.text', 'Tests ');
+  });
+
+  it('Load executions list', () => {
+    cy.get('a[href="/dashboard/executions"]:first').click();
+    cy.get('h1').should('have.text', 'Executions ');
   });
 });
