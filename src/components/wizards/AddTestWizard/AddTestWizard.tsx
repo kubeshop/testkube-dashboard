3c4e52cf3cfb5f03d9f252a90a3a11ecf3f6ab57
ediv <ediv@opinov8.com>
2022-05-12 19:01:50 +0200

    feat: finish styling and code adjustments

--- src/components/wizards/AddTestWizard/AddTestWizard.tsx
+++ src/components/wizards/AddTestWizard/AddTestWizard.tsx
@@ -161,6 +161,7 @@ const AddTestWizard: React.FC<WizardComponentProps> = props => {
       form.setFieldsValue({
         file: null,
       });
+
       form.validateFields(['file']);
     } else {
       const readFile = new FileReader();
