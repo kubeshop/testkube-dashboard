d61c15f4c547b23f852cfbcd0904cd19b3515f82
Pavlo Burchak <paul114burchak@gmail.com>
2022-08-05 12:21:03 +0300

    ginkgo icons + default icon fix

--- src/components/atoms/TestRunnerIcon/TestRunnerIcon.tsx
+++ src/components/atoms/TestRunnerIcon/TestRunnerIcon.tsx
@@ -5,13 +5,14 @@ import {TestExecutor} from '@models/testExecutors';
 import {ReactComponent as ArtilleryIcon} from '@assets/artilleryIcon.svg';
 import {ReactComponent as CurlIcon} from '@assets/curlIcon.svg';
 import {ReactComponent as CypressIcon} from '@assets/cypressIcon.svg';
+import {ReactComponent as GinkgoIcon} from '@assets/ginkgoIcon.svg';
 import {ReactComponent as GradleIcon} from '@assets/gradleIcon.svg';
 import {ReactComponent as K6Icon} from '@assets/k6Icon.svg';
 import {ReactComponent as KubepugIcon} from '@assets/kubepug.svg';
 import {ReactComponent as MavenIcon} from '@assets/mavenIcon.svg';
 import {ReactComponent as PostmanIcon} from '@assets/postmanIcon.svg';
 import {ReactComponent as SoapUIIcon} from '@assets/soapIcon.svg';
-import {ReactComponent as UnknownIcon} from '@assets/unknownIcon.svg';
+import {ReactComponent as DefaultIcon} from '@assets/tests-icon.svg';
 
 import {StyledTestRunnerIcon} from './TestRunnerIcon.styled';
 
@@ -34,13 +35,16 @@ export const testRunnerIcons: {[key in TestExecutor]: any} = {
   'maven:jdk18/test': <MavenIcon />,
   'maven/test': <MavenIcon />,
   'kubepug/yaml': <KubepugIcon />,
-  unknown: <UnknownIcon />,
+  'ginkgo/test': <GinkgoIcon />,
+  unknown: <DefaultIcon />,
 };
 
 const TestRunnerIcon: React.FC<TestRunnerIconProps> = props => {
   const {icon, noWidth = false} = props;
 
-  return <StyledTestRunnerIcon $noWidth={noWidth}>{testRunnerIcons[icon]}</StyledTestRunnerIcon>;
+  const renderIcon = icon ? testRunnerIcons[icon] : <DefaultIcon />;
+
+  return <StyledTestRunnerIcon $noWidth={noWidth}>{renderIcon}</StyledTestRunnerIcon>;
 };
 
 export default TestRunnerIcon;
