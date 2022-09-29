3e99dd0c3b9ec9c24d539786dd6a88561d0fc0ba
Toxer10 <25991946+Toxer10@users.noreply.github.com>
2022-09-29 22:25:20 +0200

    feat: added a possibility to specify a PathPrefix in Ingress controller (#448)
    * feat: added a possibility to specify a PathPrefix in Ingress controller
    
    * tech: added a comment regarding basename implementation
    
    * feat: added a new env var to scripts.sh and handled it on the frontend
    
    * tech: adjusted the code style
    
    * fix: handled ws security protocol logic (#450)
    
    Co-authored-by: ediv <ediv@opinov8.com>
    
    * fix: fixed the situation when the user was not able to add a test using git file (#451)
    
    * fix: fixed the situation when the user was not able to add a test using git file
    
    * tech: updated the app version
    
    * fix: deleted redundant if
    
    Co-authored-by: ediv <ediv@opinov8.com>
    
    * Pavlo/labels component (#449)
    
    * feat: test creation feature
    
    * feat: finish styling and code adjustments
    
    * labels component
    
    * labels component completed
    
    * some small fixes and labels added to suites
    
    * create test with labels feature
    
    * version change
    
    * types definition
    
    * lockfile
    
    Co-authored-by: ediv <ediv@opinov8.com>
    
    * lockfile
    
    * locakfile2
    
    Co-authored-by: ediv <ediv@opinov8.com>
    Co-authored-by: Pavlo Burchak <paul114burchak@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,7 @@ import {GlobalStyle} from '@styles/globalStyles';
 import {AnalyticsProvider} from './AnalyticsProvider';
 import App from './App';
 import './antd-theme/antd-customized.css';
+import env from './env';
 
 (async () => {
   const isAdBlockEnabled = await detectAdBlock();
@@ -29,10 +30,23 @@ import './antd-theme/antd-customized.css';
   const container = document.getElementById('root');
   const root = createRoot(container!);
 
+  const basename = env?.basename || '';
+
+  // If the user wants to specify a PathPrefix in Ingress controller we should
+  // set a basename to BrowserRouter. But since react-router-dom v6 they stopped
+  // mounting the router if the url does not contain this basename saying
+  // "<Router basename="/basename"> is not able to match the URL e.g. "/" because it does not start with
+  // the basename, so the <Router> won't render anything." So if we want to visit a website without knowing
+  // that we should add some basename we will not be able to see anything.
+  // Big thread here https://github.com/remix-run/react-router/issues/8427
+  if (window.location.pathname !== basename && !window.location.pathname.startsWith(basename)) {
+    window.history.pushState({}, '', basename);
+  }
+
   root.render(
     <React.StrictMode>
       <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename={basename}>
           <AnalyticsProvider privateKey={segmentIOKey}>
             <GlobalStyle />
             <App />
