136d7279c265aff1f5c16c2c9a5912782428b58a
Pavlo Burchak <paul114burchak@gmail.com>
2023-01-20 12:26:03 +0200

    added terser (#512)

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,14 @@
 const CracoAlias = require('craco-alias');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
 
 module.exports = {
+  webpack: {
+    configure: webpackConfig => {
+      webpackConfig.optimization.minimizer = [new TerserWebpackPlugin({sourceMap: false, parallel: true})];
+      webpackConfig.optimization.minimize = true;
+      return webpackConfig;
+    },
+  },
   plugins: [
     {
       plugin: CracoAlias,
