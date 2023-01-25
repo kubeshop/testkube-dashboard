82b89f3046747bdd39313a442ea2779a4d9bf389
Konrad Szałwiński <konrad.szalwinski@gmail.com>
2023-01-25 03:03:57 +0100

    [#2876] Time calculation

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,43 +1,33 @@
-import {Duration, format} from 'date-fns';
+import {Duration, format} from 'date-fns'
 
-import {executionDateFormat} from './strings';
+import {executionDateFormat} from './strings'
 
 export const formatExecutionDate = (date: Date, formatString = executionDateFormat) => {
-  return format(date, formatString);
-};
+  return format(date, formatString)
+}
 
 export const constructExecutedString = (duration: Duration, countToDisplay: number = 2) => {
-  let finishString = '';
-  let counter = 0;
+  let finishString = ''
+  let counter = 0
 
   Object.entries(duration).forEach(([key, value]) => {
-    /* Here we assume that Duration entity has a stable order of fields as said in their docs
-      type Duration = {
-        years?: number
-        months?: number
-        weeks?: number
-        days?: number
-        hours?: number
-        minutes?: number
-        seconds?: number
-      }
-    */
+
     if (value && counter < countToDisplay) {
-      finishString += `${value}${key[0]} `;
-      counter += 1;
+      finishString += `${value}${key[0]} `
+      counter += 1
     }
-  });
+  })
 
   if (!finishString.length) {
-    return '0s';
+    return '0s'
   }
 
-  return finishString.trim();
-};
+  return finishString.trim()
+}
 
 export const formatDuration = (duration: number) =>
   duration > 3599
     ? `${Math.floor(duration / 3600)}h ${Math.round(duration % 60)}m ${Math.round(duration % 3600)}s`
     : duration > 59
-    ? `${Math.floor(duration / 60)}m ${Math.round(duration % 60)}s`
-    : `${duration.toFixed(2)}s`;
+      ? `${Math.floor(duration / 60)}m ${Math.round(duration % 60)}s`
+      : `${duration.toFixed(2)}s`
