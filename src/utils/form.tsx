c15613ef00c3531fbf2b5045186a0a1aa80726b0
Bogdan Hanea <bogdan.hanea@yahoo.com>
2023-02-01 14:48:31 +0200

    feat: improve k8s pattern message (#522)

--- src/utils/form.tsx
+++ src/utils/form.tsx
@@ -19,7 +19,11 @@ const {TextArea} = Input;
 export const required: Rule = {required: true, message: 'Required.'};
 export const digits: Rule = {pattern: /\d/, message: 'Only digits.'};
 export const requiredNoText: Rule = {required: true, message: ''};
-export const k8sResourceNamePattern: Rule = {pattern: k8sResourceNameRegex, message: 'Wrong k8s name pattern'};
+export const k8sResourceNamePattern: Rule = {
+  pattern: k8sResourceNameRegex,
+  message: `
+Name must must only consist of lower case alphanumeric characters, '-' or '.', and must start and end with an alphanumeric character`,
+};
 export const k8sResourceNameMaxLength: Rule = {max: 63, message: 'Max length 63 symbols'};
 export const url: Rule = {type: 'url'};
 export const duplicateKeyMessage = 'Duplicate key.';
