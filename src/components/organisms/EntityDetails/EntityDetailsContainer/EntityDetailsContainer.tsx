5595edd236e488f0f34e5944f3304a3c446fddb5
Konrad Szałwiński <konrad.szalwinski@gmail.com>
2023-02-09 12:53:29 +0100

    Automatically switch to the right page

--- src/components/organisms/EntityDetails/EntityDetailsContainer/EntityDetailsContainer.tsx
+++ src/components/organisms/EntityDetails/EntityDetailsContainer/EntityDetailsContainer.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useLayoutEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
 import useWebSocket from 'react-use-websocket';
 
