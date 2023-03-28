02c2ee17eddbb538b038be71a13399e8b372997a
Dawid Rusnak <dawid@drcode.pl>
2023-03-28 13:37:15 +0200

    add helper to build valid URL from API endpoint

--- src/services/apiEndpoint.ts
+++ src/services/apiEndpoint.ts
@@ -3,6 +3,8 @@ import {useUnmount, useUpdate} from 'react-use';
 
 import {config} from '@constants/config';
 
+import {hasProtocol} from '@utils/strings';
+
 import env from '../env';
 
 type ApiEndpointListener = (apiEndpoint: string | null) => void;
@@ -13,11 +15,28 @@ const listeners = new Set<ApiEndpointListener>();
 // When localStorage is not working,
 // we'd like to keep it at least for the current session.
 // TODO: Consider storing it in cookie or URL params as a fallback.
-let cachedApiEndpoint: string | null = localStorage.getItem(config.apiEndpoint);
+let cachedApiEndpoint: string | null = sanitizeApiEndpoint(localStorage.getItem(config.apiEndpoint));
 
 function notifySubscriptions(): void {
   const endpoint = getApiEndpoint();
   listeners.forEach((listener) => listener(endpoint));
+}
+
+export function sanitizeApiEndpoint(apiEndpoint: string): string;
+export function sanitizeApiEndpoint(apiEndpoint: null | undefined): null;
+export function sanitizeApiEndpoint(apiEndpoint: string | null | undefined): string | null;
+export function sanitizeApiEndpoint<T>(apiEndpoint: string | null | undefined): string | null {
+  if (apiEndpoint == null) {
+    return null;
+  }
+
+  if (!hasProtocol(apiEndpoint)) {
+    apiEndpoint = `${window.location.protocol}//${apiEndpoint}`;
+  }
+
+  return apiEndpoint
+    .replace(/\/+$/, '')
+    .replace(/(\/v1)?$/, '/v1');
 }
 
 export function getApiEndpoint(): string | null {
@@ -26,8 +45,8 @@ export function getApiEndpoint(): string | null {
 
 export function saveApiEndpoint(apiEndpoint: string): boolean {
   try {
-    cachedApiEndpoint = apiEndpoint;
-    localStorage.setItem(config.apiEndpoint, apiEndpoint);
+    cachedApiEndpoint = sanitizeApiEndpoint(apiEndpoint);
+    localStorage.setItem(config.apiEndpoint, cachedApiEndpoint);
     notifySubscriptions();
     return true;
   } catch (e) {
--- src/utils/endpoint.ts
+++ src/utils/endpoint.ts
@@ -1,21 +1,5 @@
-import {hasProtocol} from '@utils/strings';
+import {sanitizeApiEndpoint} from '@services/apiEndpoint';
 
 export const checkAPIEndpoint = (apiEndpoint: string, checkURLWorkingState: (apiEndpoint: string) => void) => {
-  const endsWithV1 = apiEndpoint.endsWith('/v1');
-
-  if (hasProtocol(apiEndpoint)) {
-    if (endsWithV1) {
-      checkURLWorkingState(`${apiEndpoint}/info`);
-    } else {
-      checkURLWorkingState(`${apiEndpoint}/v1/info`);
-    }
-  } else {
-    const targetProtocol = `${window.location.protocol}//`;
-
-    if (endsWithV1) {
-      checkURLWorkingState(`${targetProtocol}${apiEndpoint}/info`);
-    } else {
-      checkURLWorkingState(`${targetProtocol}${apiEndpoint}/v1/info`);
-    }
-  }
+  checkURLWorkingState(`${sanitizeApiEndpoint(apiEndpoint)}/info`);
 };
