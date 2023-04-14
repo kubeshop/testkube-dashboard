ec0b82ceb3e1d1651ef4b75df1cf7ab1d2e48e1a
Pravesh <88569706+Pravesh-Sudha@users.noreply.github.com>
2023-04-14 19:00:04 +0530

    Update Command.tsx

--- src/components/pages/Executors/ExecutorDetails/ExecutorSettings/CommandAndArguments/Command.tsx
+++ src/components/pages/Executors/ExecutorDetails/ExecutorSettings/CommandAndArguments/Command.tsx
@@ -9,7 +9,6 @@ import {selectCurrentExecutor, updateExecutorCommand} from '@redux/reducers/exec
 
 import {ConfigurationCard, notificationCall} from '@molecules';
 
-import {required} from '@utils/form';
 import {displayDefaultErrorNotification} from '@utils/notification';
 
 import {useUpdateCustomExecutorMutation} from '@services/executors';
