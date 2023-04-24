8f725a99b779cbebda8b387f17c895a65b5bc795
Dawid Rusnak <dawid@drcode.pl>
2023-04-24 17:46:05 +0200

    flatten composing providers

---
+++ src/utils/composeProviders.tsx
@@ -0,0 +1,22 @@
+import {createElement, FC, ReactElement} from 'react';
+
+class ProviderCompositor {
+  public providers: {provider: FC<any>, props: any}[] = [];
+
+  public append<T>(provider: FC<T>, props: Omit<T, 'children'>): this {
+    this.providers.push({provider, props});
+    return this;
+  }
+
+  public render(children: ReactElement): ReactElement {
+    let current = children;
+    for (let i = this.providers.length - 1; i >= 0; i -= 1) {
+      current = createElement(this.providers[i].provider, this.providers[i].props, current);
+    }
+    return current;
+  }
+}
+
+export function composeProviders(): ProviderCompositor {
+  return new ProviderCompositor();
+}
