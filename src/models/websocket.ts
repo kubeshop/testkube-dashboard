2e19ee7e4b997ae7cf1d4bcb304729e8e938f16f
Dawid Rusnak <dawid@drcode.pl>
2023-04-25 11:18:30 +0200

    Unify EntityDetailsContainer (#618)
    * chore: unify RTK implementation with Cloud version
    * fixup align types
    * feat: expose getters for internal RTK values
    * fixup
    * chore: align EntityDetailsContainer with Cloud

--- src/models/websocket.ts
+++ src/models/websocket.ts
@@ -1,11 +1,22 @@
 export enum WSEventType {
   START_TEST = 'start-test',
+  START_TEST_SUITE = 'start-testsuite',
+
   END_TEST_SUCCESS = 'end-test-success',
+  END_TEST_SUITE_SUCCESS = 'end-testsuite-success',
+
+  END_TEST_FAILED = 'end-test-failed',
+  END_TEST_SUITE_FAILED = 'end-testsuite-failed',
+
   END_TEST_ABORT = 'end-test-aborted',
   END_TEST_TIMEOUT = 'end-test-timeout',
+
+  END_TEST_SUITE_ABORT = 'end-testsuite-aborted',
+  END_TEST_SUITE_TIMEOUT = 'end-testsuite-timeout',
 }
 
 export type WSData = {
   type: WSEventType;
   testExecution?: any;
+  testSuiteExecution?: any;
 };
