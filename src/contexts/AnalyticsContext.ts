d5acf8a21bcd790f0f59e63ca3eb85da4b054d2f
Bogdan Hanea <bogdan.hanea@yahoo.com>
2023-05-03 16:15:33 +0300

    feat: added support for feature flags

--- src/contexts/AnalyticsContext.ts
+++ src/contexts/AnalyticsContext.ts
@@ -5,6 +5,7 @@ import {AnalyticsBrowser} from '@segment/analytics-next';
 const AnalyticsContext = createContext<{
   analytics?: AnalyticsBrowser;
   analyticsTrack: (type: string, data: any) => void;
+  featureFlags: string[];
 }>(undefined!);
 
 export default AnalyticsContext;
