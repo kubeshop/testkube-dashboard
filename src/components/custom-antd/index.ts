7724c62ce6458f33e9974bb5e782085456b1979d
Toxer10 <25991946+Toxer10@users.noreply.github.com>
2023-05-12 09:57:29 +0200

    feat: added a custom space component (#660)
    * feat: added a custom space component
    
    * feat: renamed the component according to comment on PR
    
    ---------
    
    Co-authored-by: ediv <ediv@opinov8.com>

--- src/components/custom-antd/index.ts
+++ src/components/custom-antd/index.ts
@@ -7,3 +7,4 @@ export {default as FormItem} from './Form/FormItem';
 export {default as Upload} from './Upload';
 export {default as Modal} from './Modal';
 export {default as Checkbox} from './Checkbox';
+export {default as FullWidthSpace} from './FullWidthSpace';
