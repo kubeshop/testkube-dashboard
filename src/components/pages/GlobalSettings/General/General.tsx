232e7836d28d67fe4a675227dc1b568557f7fffb
Bogdan Hanea <bogdan.hanea@yahoo.com>
2023-05-15 14:21:28 +0300

    feat: connect to testkube cloud (#667)

--- src/components/pages/GlobalSettings/General/General.tsx
+++ src/components/pages/GlobalSettings/General/General.tsx
@@ -1,11 +1,13 @@
 import {Space} from 'antd';
 
 import ApiEndpoint from './ApiEndpoint';
+import ConnectCloud from './ConnectCloud';
 
 const GeneralSettings = () => {
   return (
     <Space size={30} direction="vertical">
       <ApiEndpoint />
+      <ConnectCloud />
     </Space>
   );
 };
