2d22d892e4f17fb59eee06e6820ebe0af3e86064
Tomasz Konieczny <tkonieczny91@gmail.com>
2023-05-16 11:08:33 +0200

    feat: dashboard E2E tests migrated from main repo (#669)
    * E2E tests migrated from main repo
    
    * E2E tests migrated from main repo - dir change
    
    * updated after linter
    
    * updated after linter
    
    * updated after linter
    
    * typo fixed
    
    * semicolons
    
    * fixes after eslint
    
    * fixes after eslint

---
+++ test/dashboard-e2e/tests/create-test.spec.ts
@@ -0,0 +1,51 @@
+import { test } from '@playwright/test';
+
+import { TestDataHandler } from '../data-handlers/test-data-handlers';
+import { ApiHelpers } from '../api/api-helpers';
+import { CommonHelpers } from '../helpers/common-helpers';
+import { MainPage } from '../pages/MainPage';
+import { CreateTestPage } from '../pages/CreateTestPage';
+
+test.beforeEach(async ({ page }) => {
+  await page.addInitScript(() => {
+    window.localStorage.setItem('isGADisabled', '1');
+  });
+});
+
+const testNames = ['cypress-git', 'k6-git', 'postman-git'];
+for (const testName of testNames) { // eslint-disable-line no-restricted-syntax
+  test(`Creating test for ${testName}`, async ({ page }) => {
+    const testData = TestDataHandler.getTest(testName);
+  
+    const apiHelpers=new ApiHelpers(process.env.API_URL);
+    await apiHelpers.assureTestNotCreated(testData.name);
+    const mainPage=new MainPage(page);
+    await mainPage.visitMainPage();
+    await mainPage.openCreateTestDialog();
+  
+    const createTestPage=new CreateTestPage(page);
+    await createTestPage.createTest(testName);
+  
+    await page.waitForURL(`**/tests/executions/${testData.name}`);
+  
+    const createdTestData = await apiHelpers.getTestData(testData.name);
+  
+    await CommonHelpers.validateTest(testData, createdTestData);
+  });
+}
+
+test.skip(`Create test from File`, async ({ page }) => {
+
+});
+
+test.skip(`Create test from String`, async ({ page }) => {
+
+});
+
+test.skip(`Create test from Git source`, async ({ page }) => {
+
+});
+
+test.skip(`Create test with Labels`, async ({ page }) => {
+
+});
