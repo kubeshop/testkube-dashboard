6d9ad1cae7c1942fb76bc1a28efe326de690f1e2
Tomasz Konieczny <tkonieczny91@gmail.com>
2023-05-16 12:14:59 +0200

    test

--- test/dashboard-e2e/pages/CreateTestPage.ts
+++ test/dashboard-e2e/pages/CreateTestPage.ts
@@ -1,5 +1,5 @@
 import type { Page } from  '@playwright/test';
-import { TestDataHandler } from '../data-handlers/test-data-handlers';
+import TestDataHandler from '../data-handlers/test-data-handlers';
 
 export class CreateTestPage{
     readonly page: Page;
