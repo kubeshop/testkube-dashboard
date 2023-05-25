b54bceb03fe02ac5659833e0fddf03622d284ad7
ediv <ediv@opinov8.com>
2023-05-25 16:38:30 +0200

    tech: changes due to comments on PR

--- src/components/custom-antd/FullWidthSpace/FullWidthSpace.styled.tsx
+++ src/components/custom-antd/FullWidthSpace/FullWidthSpace.styled.tsx
@@ -6,8 +6,8 @@ export interface FullWidthSpaceProps extends SpaceProps {
   justify?: 'flex-start' | 'flex-end' | 'center' | 'space-around' | 'space-between';
 }
 
-export const StyledSpace = styled(AntdSpace)<{justify: FullWidthSpaceProps['justify']}>`
-  justify-content: ${({justify}) => justify};
+export const StyledSpace = styled(AntdSpace)<{$justify: FullWidthSpaceProps['justify']}>`
+  justify-content: ${({$justify}) => $justify};
 
   width: 100%;
 `;
--- src/components/custom-antd/FullWidthSpace/FullWidthSpace.tsx
+++ src/components/custom-antd/FullWidthSpace/FullWidthSpace.tsx
@@ -3,7 +3,7 @@ import {FullWidthSpaceProps, StyledSpace} from './FullWidthSpace.styled';
 const FullWidthSpace: React.FC<FullWidthSpaceProps> = props => {
   const {justify = 'flex-start', ...rest} = props;
 
-  return <StyledSpace justify={justify} {...rest} />;
+  return <StyledSpace $justify={justify} {...rest} />;
 };
 
 export default FullWidthSpace;
