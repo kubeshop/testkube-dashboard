111921d3a85b3e89e444ebd5d4c98b0ba9a1df23
Pavlo Burchak <paul114burchak@gmail.com>
2023-05-30 16:35:06 +0300

    prettier fix

--- src/store/TriggersLocalStore.ts
+++ src/store/TriggersLocalStore.ts
@@ -1,25 +1,27 @@
-import {useRef} from "react";
+import {useRef} from 'react';
+
 import {create} from 'zustand';
 import {devtools} from 'zustand/middleware';
 import {shallow} from 'zustand/shallow';
+
 import createTriggersSlice, {TriggersSlice} from './TriggersSlice';
 
 const useTriggersLocalStore = () => {
-    const useStoreRef = useRef(
-        create<TriggersSlice>()(
-            devtools(
-              (...a) => ({
-                ...createTriggersSlice(...a),
-              }),
-              {name: 'Triggers Local Store Zustand', enabled: process.env.NODE_ENV === 'development'}
-            )
-          )
-    );
-    const useStore = useStoreRef.current;
-          
-    const useShallowLocalStore = <U>(selector: (state: TriggersSlice) => U) => useStore(selector, shallow);
+  const useStoreRef = useRef(
+    create<TriggersSlice>()(
+      devtools(
+        (...a) => ({
+          ...createTriggersSlice(...a),
+        }),
+        {name: 'Triggers Local Store Zustand', enabled: process.env.NODE_ENV === 'development'}
+      )
+    )
+  );
+  const useStore = useStoreRef.current;
 
-    return [useShallowLocalStore];
-}; 
+  const useShallowLocalStore = <U>(selector: (state: TriggersSlice) => U) => useStore(selector, shallow);
+
+  return [useShallowLocalStore];
+};
 
 export default useTriggersLocalStore;
