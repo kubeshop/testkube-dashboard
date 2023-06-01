982425d06470ab99907e62be3c72578efa6c83d6
Dawid Rusnak <dawid@drcode.pl>
2023-06-01 16:29:26 +0200

    feat: add overrides mechanism for testing variables (#716)
    * feat: add overrides mechanism for testing variables
    
    Resolves: kubeshop/testkube#3948
    
    * typo: rename hasOverridesApplied to areOverridesApplied

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -2,4 +2,6 @@ export const config = {
   apiEndpoint: 'apiEndpoint',
   apiVersion: '/v1/executions',
   isTestkubeCloudLaunchBannerHidden: 'isTestkubeCloudLaunchBannerHidden',
+
+  overrides: '$tkOverrides',
 };
