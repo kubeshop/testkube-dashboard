8358bf6accc1f379e0815b7840a840822188383e
Dawid Rusnak <dawid@drcode.pl>
2023-06-05 08:50:00 +0200

    feat: make EntityGrid more generic and full-featured

--- src/components/molecules/EntityGrid/EntityGrid.tsx
+++ src/components/molecules/EntityGrid/EntityGrid.tsx
@@ -1,25 +1,74 @@
-import {memo, useMemo} from 'react';
+import React, {FC, ReactElement, memo, useMemo} from 'react';
+
+import {ScrollTrigger} from '@atoms';
+
+import EntityListLoader from '@organisms/EntityList/EntityListContent/EntityListLoader';
+import EntityListSkeleton from '@organisms/EntityList/EntityListContent/EntityListSkeleton';
 
 import {StyledEntityGrid} from './EntityGrid.styled';
-import EntityGridItem from './EntityGridItem';
 
-type EntityGridProps = {
-  data: any[];
-  onNavigateToDetails: any;
+type OptionalProp<T, U extends keyof T> = Omit<T, U> & Partial<Pick<T, U>>;
+
+type BaseEntityGridProps<T extends {item: any}> = {
+  data?: T['item'][];
+  Component: FC<T>;
+  componentProps: Omit<T, 'item'>;
+  itemHeight?: number;
+  scrollOffset?: number;
+  empty: ReactElement<any, any>;
+  loadingInitially?: boolean;
+  loadingMore?: boolean;
+  hasMore?: boolean;
+  onScrollEnd?: () => void;
 };
 
-const EntityGrid: React.FC<EntityGridProps> = props => {
-  const {data, onNavigateToDetails} = props;
+type EntityGridProps<T extends {item: any}> = {} extends Omit<T, 'item' | 'key'>
+  ? OptionalProp<BaseEntityGridProps<T>, 'componentProps'>
+  : BaseEntityGridProps<T>;
 
-  const renderedGrid = useMemo(() => {
-    return data.map((item: any, index) => {
-      const key = `entity_grid_item-${index}`;
+function EntityGrid<T extends {item: any}>(props: EntityGridProps<T>): ReactElement<any, any> {
+  const {
+    data,
+    Component,
+    componentProps,
+    itemHeight = 150,
+    scrollOffset,
+    empty,
+    loadingInitially = false,
+    loadingMore = false,
+    hasMore = false,
+    onScrollEnd,
+  } = props;
 
-      return <EntityGridItem item={item} onClick={onNavigateToDetails} key={key} />;
-    });
-  }, [data]);
+  const elements = useMemo(
+    () =>
+      data?.map((item, index) => {
+        const itemProps = {...componentProps, item} as T;
+        // eslint-disable-next-line react/no-array-index-key
+        return <Component {...itemProps} key={`entity_grid_item-${index}`} />;
+      }),
+    [data]
+  );
 
-  return <StyledEntityGrid>{renderedGrid}</StyledEntityGrid>;
-};
+  if (loadingInitially) {
+    return <EntityListSkeleton height={itemHeight} />;
+  }
+
+  if (!loadingInitially && !data?.length) {
+    return empty;
+  }
+
+  return (
+    <>
+      <StyledEntityGrid>{elements}</StyledEntityGrid>
+      <ScrollTrigger
+        offset={scrollOffset ?? itemHeight * 1.5}
+        disabled={!hasMore || loadingMore}
+        onScroll={onScrollEnd}
+      />
+      {loadingMore ? <EntityListLoader /> : null}
+    </>
+  );
+}
 
 export default memo(EntityGrid);
