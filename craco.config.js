710b5ecb67f271051fffae17dd1985ba0c3b0d22
Pavlo Burchak <paul114burchak@gmail.com>
2023-06-27 11:17:35 +0300

    monaco plugin features

--- craco.config.js
+++ craco.config.js
@@ -18,6 +18,7 @@ module.exports = {
               },
             },
           ],
+          features: ['caretOperations', 'clipboard', 'contextmenu', 'hover', 'indentation', 'lineSelection', 'suggest']
         }),
       ],
     },
