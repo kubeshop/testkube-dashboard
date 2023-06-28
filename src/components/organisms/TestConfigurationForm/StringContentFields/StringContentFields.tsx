c3fdad750e8b416eb407920863c668d76444c341
Pavlo Burchak <paul114burchak@gmail.com>
2023-06-28 11:49:01 +0300

    placeholders for string (#752)

--- src/components/organisms/TestConfigurationForm/StringContentFields/StringContentFields.tsx
+++ src/components/organisms/TestConfigurationForm/StringContentFields/StringContentFields.tsx
@@ -6,10 +6,37 @@ import Fonts from '@styles/Fonts';
 
 import {required} from '@utils/form';
 
-const StringContentFields: React.FC = () => {
+import {Props} from '../utils';
+
+export const stringPlaceholders: Record<string, string> = {
+  curl: `e.g.:
+{
+  "command": [
+    "curl",
+    "https://reqbin.com/echo/get/json",
+    "-H",
+    "'Accept: application/json'"
+  ],
+  "expected_status": "200",
+  "expected_body": "{\"success\":\"true\"}"
+}`,
+  k6: `e.g.:
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export default function () {
+  http.get('https://test.k6.io');
+  sleep(1);
+}`,
+};
+
+const StringContentFields: React.FC<Partial<Props>> = props => {
+  const {executorType} = props;
+
+  const placeholder = stringPlaceholders[executorType!] || 'String...';
   return (
     <FormItem name="string" label="String" rules={[required]} required>
-      <TextArea rows={10} placeholder="String..." style={{fontFamily: Fonts.robotoMono}} />
+      <TextArea rows={11} placeholder={placeholder} style={{fontFamily: Fonts.robotoMono}} />
     </FormItem>
   );
 };
