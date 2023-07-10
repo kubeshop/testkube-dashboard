7e9aa9cb6bed9b36e0b7b4a65ffd6bebc4c39b7a
Dawid Rusnak <dawid@drcode.pl>
2023-07-10 12:59:17 +0200

    feat: simplify building env config

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
-# line endings must be \n, not \r\n !
-echo "window._env_ = {" >./env-config.js
-echo "REACT_APP_API_SERVER_ENDPOINT: '${REACT_APP_API_SERVER_ENDPOINT}'," >>./env-config.js
-echo "REACT_APP_ROOT_ROUTE: '${REACT_APP_ROOT_ROUTE}'," >>./env-config.js
-echo "REACT_APP_DISABLE_TELEMETRY: '${REACT_APP_DISABLE_TELEMETRY}'," >>./env-config.js
-echo "};" >>./env-config.js
+
+OUTPUT_FILE=env-config.js
+VARIABLES=(
+  REACT_APP_API_SERVER_ENDPOINT
+  REACT_APP_ROOT_ROUTE
+  REACT_APP_DISABLE_TELEMETRY
+  REACT_APP_CRD_OPERATOR_REVISION
+)
+
+echo "window._env_ = {" > "$OUTPUT_FILE"
+for name in ${VARIABLES[@]}; do
+  value="$(eval "echo \"\$${name}\"")"
+  echo "${name}: '${value}'," >> "$OUTPUT_FILE"
+done
+echo "};" >> "$OUTPUT_FILE"
