76e337ea683cd86d185387a140a864875eafd7d4
Toxer10 <25991946+Toxer10@users.noreply.github.com>
2023-07-13 21:19:34 +0200

    tests for strings util (#775)
    * tests for strings util
    
    * added more tests for strings util
    
    ---------
    
    Co-authored-by: ediv <ediv@opinov8.com>

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -4,9 +4,9 @@ export const k8sResourceNameRegex = /^[a-z0-9]{0,1}[a-z0-9-.]+[a-z0-9]{1}$/;
 export const secretRegex = /^[^*]+$/;
 
 export const hasProtocol = (url: string) => {
-  return /^http(s)?:\/\/.*/.test(url);
+  return /^(?:(http|ftp)(s?)|ssh)\:\/\//.test(url);
 };
 
 export const isURL = (url: string) => {
-  return /^http(s)?:\/\/.*/.test(url);
+  return /^(?:(http|ftp)(s?)|ssh)\:\/\//.test(url);
 };
