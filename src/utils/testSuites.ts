260cf0e413b680ef7445df727597708f62532465
Dawid Rusnak <dawid@drcode.pl>
2023-07-14 17:48:35 +0200

    support displaying test suite V2 execution steps

--- src/utils/testSuites.ts
+++ src/utils/testSuites.ts
@@ -1,7 +1,17 @@
 import {TestSuite, TestSuiteStep} from '@models/testSuite';
+import {TestSuiteExecution} from '@models/testSuiteExecution';
+
+type TestSuiteExecutionMaybeV2 = TestSuiteExecution & {stepResults?: any};
 
 export function isTestSuiteV2(suite: TestSuite): boolean {
   return !Array.isArray(suite.steps?.[0]?.execute || []);
+}
+
+function mapExecute(execute: any): TestSuiteStep[] {
+  if (!execute) {
+    return [];
+  }
+  return ([] as TestSuiteStep[]).concat(execute!).map(item => ('name' in item ? {test: item.name as string} : item));
 }
 
 export function convertTestSuiteV2ToV3(suite: TestSuite): TestSuite {
@@ -9,9 +19,22 @@ export function convertTestSuiteV2ToV3(suite: TestSuite): TestSuite {
     ...suite,
     steps: suite.steps?.map(step => ({
       ...step,
-      execute: ([] as TestSuiteStep[])
-        .concat(step.execute!)
-        .map(item => ('name' in item ? {test: item.name as string} : item)),
+      execute: mapExecute(step.execute!),
+    })),
+  };
+}
+
+export function isTestSuiteV2Execution(execution: TestSuiteExecutionMaybeV2): boolean {
+  return Boolean(execution.stepResults);
+}
+
+export function convertTestSuiteV2ExecutionToV3(execution: TestSuiteExecutionMaybeV2): TestSuiteExecution {
+  const {stepResults, ...rest} = execution;
+  return {
+    ...rest,
+    executeStepResults: stepResults.map((result: any) => ({
+      execute: [{execution: result.execution, step: mapExecute(result.step.execute)?.[0]}],
+      step: {...result.step, execute: mapExecute(result.step.execute)},
     })),
   };
 }
