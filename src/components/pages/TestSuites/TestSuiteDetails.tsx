8de5bae15c9afbaad9f2d7a7f102c968c5c1a243
Dawid Rusnak <dawid@drcode.pl>
2023-08-01 10:15:36 +0200

    fixup prettier

--- src/components/pages/TestSuites/TestSuiteDetails.tsx
+++ src/components/pages/TestSuites/TestSuiteDetails.tsx
@@ -2,8 +2,6 @@ import {FC} from 'react';
 
 import {EntityDetailsContainer} from '@organisms';
 
-const TestSuiteDetails: FC = () => (
-  <EntityDetailsContainer entity="test-suites" />
-);
+const TestSuiteDetails: FC = () => <EntityDetailsContainer entity="test-suites" />;
 
 export default TestSuiteDetails;
--- src/components/pages/Tests/TestDetails.tsx
+++ src/components/pages/Tests/TestDetails.tsx
@@ -2,8 +2,6 @@ import {FC} from 'react';
 
 import {EntityDetailsContainer} from '@organisms';
 
-const TestDetails: FC = () => (
-  <EntityDetailsContainer entity="tests" />
-);
+const TestDetails: FC = () => <EntityDetailsContainer entity="tests" />;
 
 export default TestDetails;
