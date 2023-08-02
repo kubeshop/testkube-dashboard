c8dbb2593af83ba93bb646d0b5baeb8800eafcc7
Bogdan Hanea <bogdan.hanea@yahoo.com>
2023-08-02 16:48:34 +0300

    change plugin slot name

--- src/components/pages/TestSuites/TestSuitesList/TestSuitesList.tsx
+++ src/components/pages/TestSuites/TestSuitesList/TestSuitesList.tsx
@@ -69,7 +69,7 @@ const TestSuitesList: FC = () => {
       onItemAbort={onItemAbort}
       entity="test-suites"
       pageTitle="Test Suites"
-      pageTitleAddon={usePluginSlot('testSuiteTitleIcon')}
+      pageTitleAddon={usePluginSlot('testSuiteTitleAddon')}
       addEntityButtonText="Add a new test suite"
       pageDescription={PageDescription}
       emptyDataComponent={EmptyTestSuites}
