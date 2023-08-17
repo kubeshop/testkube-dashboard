afabd5bd38a3ec024ada6af724ecf9b2a3b0277d
Dawid Rusnak <dawid@drcode.pl>
2023-08-17 15:26:16 +0200

    chore: delete unused Executors initial state

--- src/redux/initialState.ts
+++ src/redux/initialState.ts
@@ -1,15 +1,3 @@
-import {ExecutorsState} from '@models/executors';
-
 export const initialPageSize = 20;
 
-const initialExecutorsState: ExecutorsState = {
-  executorsList: [],
-  executorsFeaturesMap: {},
-  currentExecutor: undefined,
-};
-
-const initialReduxState = {
-  executors: initialExecutorsState,
-};
-
-export default initialReduxState;
+export default {};
