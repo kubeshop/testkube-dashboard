0fc253796f6c0f6b566397221d9a82e01a17a51d
Dawid Rusnak <dawid@drcode.pl>
2023-08-18 17:09:33 +0200

    fix: add react-router integration for Sentry

--- src/sentry.ts
+++ src/sentry.ts
@@ -1,3 +1,5 @@
+import {useEffect} from 'react';
+import {useLocation, useNavigationType, createRoutesFromChildren, matchRoutes} from 'react-router-dom';
 import * as Sentry from '@sentry/react';
 
 import env from '@env';
@@ -5,5 +7,17 @@ import env from '@env';
 if (env.sentryKey) {
   Sentry.init({
     dsn: env.sentryKey,
+    integrations: [
+      new Sentry.BrowserTracing({
+        // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+        routingInstrumentation: Sentry.reactRouterV6Instrumentation(
+          useEffect,
+          useLocation,
+          useNavigationType,
+          createRoutesFromChildren,
+          matchRoutes
+        ),
+      }),
+    ]
   });
 }
