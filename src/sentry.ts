e1a7ce1f5a38b1583dd5f2f02391809c1a633bb5
Dawid Rusnak <dawid@drcode.pl>
2023-08-22 11:35:21 +0200

    fix: Sentry improvements (#845)
    * fix: add missing app version for Sentry release
    * fix: add react-router integration for Sentry
    * fix: catch errors in Sentry correctly
    * fix: disable Sentry telemetry

--- src/sentry.ts
+++ src/sentry.ts
@@ -1,9 +1,26 @@
+import {CaptureConsole} from '@sentry/integrations';
 import * as Sentry from '@sentry/react';
+
+import {useEffect} from 'react';
+import {createRoutesFromChildren, matchRoutes, useLocation, useNavigationType} from 'react-router-dom';
 
 import env from '@env';
 
 if (env.sentryKey) {
   Sentry.init({
     dsn: env.sentryKey,
+    integrations: [
+      new CaptureConsole({levels: ['error']}),
+      new Sentry.BrowserTracing({
+        // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+        routingInstrumentation: Sentry.reactRouterV6Instrumentation(
+          useEffect,
+          useLocation,
+          useNavigationType,
+          createRoutesFromChildren,
+          matchRoutes
+        ),
+      }),
+    ],
   });
 }
