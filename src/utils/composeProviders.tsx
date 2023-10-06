3a0d229f9ce187f659284bab482817d9d7ceb134
Dawid Rusnak <dawid@drcode.pl>
2023-10-06 16:16:05 +0200

    fix: composeProviders types

--- src/utils/composeProviders.tsx
+++ src/utils/composeProviders.tsx
@@ -1,15 +1,15 @@
-import {FC, ReactElement, createElement} from 'react';
+import {ReactElement, ReactNode, createElement} from 'react';
 
 class ProviderCompositor {
-  public providers: {provider: FC<any>; props: unknown}[] = [];
+  public providers: {provider: (props: any) => ReactElement | null; props: unknown}[] = [];
 
-  public append<T>(provider: FC<T>, props: Omit<T, 'children'>): this {
+  public append<T>(provider: (props: T) => ReactElement | null, props: Omit<T, 'children'>): this {
     this.providers.push({provider, props});
     return this;
   }
 
-  public render(children: ReactElement): ReactElement {
-    let current = children;
+  public render(children: ReactNode): ReactElement | null {
+    let current = children as ReactElement | null;
     for (let i = this.providers.length - 1; i >= 0; i -= 1) {
       current = createElement(this.providers[i].provider, this.providers[i].props, current);
     }
