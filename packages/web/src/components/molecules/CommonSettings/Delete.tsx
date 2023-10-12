74f5651fe38bef223cbe9619b6c05a1aa7e82a1f
Bogdan Hanea <bogdan.hanea@yahoo.com>
2023-10-12 14:05:40 +0300

    fixed after conflicts

--- packages/web/src/components/molecules/CommonSettings/Delete.tsx
+++ packages/web/src/components/molecules/CommonSettings/Delete.tsx
@@ -11,6 +11,8 @@ interface DeleteProps {
   description: string;
   label: string;
   redirectUrl: string;
+  onDelete: (id: string) => Promise<any>;
+}
 
 const Delete: FC<DeleteProps> = ({name, description, label, redirectUrl, onDelete}) => {
   const {open, close} = useModal({
--- packages/web/src/components/molecules/DeleteEntityModal/DeleteEntityModal.tsx
+++ packages/web/src/components/molecules/DeleteEntityModal/DeleteEntityModal.tsx
@@ -53,7 +53,6 @@ const DeleteEntityModal: React.FC<{
         </FullWidthSpace>
       }
     />
-
   );
 };
 
