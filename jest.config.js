68ad197c6754b31fc0f1baeacdf3678d8c4a15d9
Dawid Rusnak <dawid@drcode.pl>
2023-10-17 10:56:55 +0200

    fix: improve coverage collection for unit tests (#909)
    * fix: improve coverage collection for unit tests
    
    * feat: send Test Coverage results to the CodeClimate
    
    * fixup codeclimate-action version
    
    * chore: add CodeClimate configuration
    
    * fixup codeclimate configuration
    
    * fix: add a title for autocomplete options
    
    * fix: Triggers E2E tests
    
    * fixup adjust codeclimate configuration

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 const {dirname, resolve, join} = require('node:path');
 const {readdirSync, existsSync} = require('node:fs');
+const micromatch = require('micromatch');
+const glob = require('glob');
 
 function readConfig(filePath) {
   const absFilePath = resolve(filePath);
@@ -14,13 +16,45 @@ function readConfig(filePath) {
   return result;
 }
 
+function escapeRegexWord(filePath) {
+  return filePath.replace(/[^a-z0-9]/gi, $ => `\\${$}`);
+}
+
 const packages = readdirSync(join(__dirname, 'packages')).filter(name =>
   existsSync(join(__dirname, 'packages', name, 'jest.config.js'))
 );
 
+const projects = packages.map(name => ({
+  ...readConfig(join(__dirname, 'packages', name, 'jest.config.js')),
+  rootDir: join(__dirname, 'packages', name),
+}));
+
+const files = glob.sync(`${join(__dirname, 'packages')}/**`, {ignore: '**/node_modules/**'});
+
+const coveragePatterns = projects.map(project =>
+  (project.collectCoverageFrom || []).map(pattern => {
+    if (pattern.includes('<rootDir>')) {
+      return pattern.replace('<rootDir>', project.rootDir);
+    }
+    if (/^!?\//.test(pattern)) {
+      return pattern;
+    }
+    return pattern.startsWith('!') ? `!${project.rootDir}/${pattern.substring(1)}` : `${project.rootDir}/${pattern}`;
+  })
+);
+
+const coverageFiles = coveragePatterns.flatMap(patterns =>
+  files.filter(filePath => micromatch.isMatch(filePath, patterns))
+);
+
 module.exports = {
-  projects: packages.map(name => ({
-    ...readConfig(join(__dirname, 'packages', name, 'jest.config.js')),
-    rootDir: join(__dirname, 'packages', name),
-  })),
+  projects,
+  // Hack to inherit coverage patterns from projects
+  collectCoverageFrom: ['**/*.{ts,tsx}'],
+  coveragePathIgnorePatterns: [
+    `^(?!${escapeRegexWord(__dirname)}\\/${coverageFiles
+      .map(filePath => filePath.replace(`${__dirname}/`, ''))
+      .map(escapeRegexWord)
+      .join('|')})$).*$`,
+  ],
 };
