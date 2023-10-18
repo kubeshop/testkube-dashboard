b3c807a07d5778e4c78568adc3990a8daa6c9d91
Dawid Rusnak <dawid@drcode.pl>
2023-10-18 14:33:08 +0200

    feat: keep useSlot's results identity when not changed

--- packages/plugins/src/hooks.ts
+++ packages/plugins/src/hooks.ts
@@ -1,9 +1,9 @@
-import {useContext} from 'react';
+import {useContext, useMemo, useRef} from 'react';
 
 import type {GetPluginState, Plugin} from './internal/Plugin';
 import {PluginScope} from './internal/PluginScope';
 import {PluginScopeContext} from './internal/PluginScopeProvider';
-import type {GetSlots, PluginScopeDataRecord, PluginScopeStateFor} from './internal/types';
+import type {GetSlots, PluginScopeDataRecord, PluginScopeStateFor, PluginSlotContainer} from './internal/types';
 
 export const usePluginScope = <T extends Plugin<any>>(): PluginScope<PluginScopeStateFor<GetPluginState<T>>> =>
   useContext(PluginScopeContext).root as any;
@@ -14,8 +14,14 @@ export const createUseData =
     usePluginScope<T>().data as any;
 export const createUseSlot =
   <T extends Plugin<any>>() =>
-  <K extends keyof GetSlots<GetPluginState<T>>>(key: K): GetSlots<GetPluginState<T>>[K][] =>
-    usePluginScope<T>().slots[key]?.all() || [];
+  <K extends keyof GetSlots<GetPluginState<T>>>(key: K): GetSlots<GetPluginState<T>>[K][] => {
+    const results = usePluginScope<T>().slots[key]?.allRaw();
+    const resultsRef = useRef<PluginSlotContainer<T>[]>();
+    if (results?.length !== resultsRef.current?.length || resultsRef.current?.some((x, i) => results![i] !== x)) {
+      resultsRef.current = results;
+    }
+    return useMemo(() => resultsRef.current?.map(x => x.value) || [], [resultsRef.current]);
+  };
 export const createUseSlotFirst =
   <T extends Plugin<any>>() =>
   <K extends keyof GetSlots<GetPluginState<T>>>(key: K): GetSlots<GetPluginState<T>>[K] | undefined =>
