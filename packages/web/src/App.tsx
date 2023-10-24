c7da60e9bdaf78b754dc2444f65e9bf55c183404
Dawid Rusnak <dawid@drcode.pl>
2023-10-24 14:01:55 +0200

    feat: extract API endpoint modal to Cluster plugin

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,78 +1,21 @@
-import React, {Suspense, useEffect} from 'react';
-import {Route, Routes, useLocation} from 'react-router-dom';
+import React, {Suspense} from 'react';
+import {Route, Routes} from 'react-router-dom';
 import {useUpdate} from 'react-use';
 
 import {PluginRoute} from '@testkube/plugins/src/internal/types';
-
-import {SystemAccess, useSystemAccess} from '@hooks/useSystemAccess';
-
-import {useModal} from '@modal/hooks';
-
-import {EndpointModal, notificationCall} from '@molecules';
 
 import {Loading, NotFound} from '@pages';
 
 import {useGeneralSlot} from '@plugins/general/hooks';
 
-import {getApiDetails, getApiEndpoint, isApiEndpointLocked, useApiEndpoint} from '@services/apiEndpoint';
-
 import {BannerList} from '@src/App.styled';
-
-import {useClusterDetailsPick} from '@store/clusterDetails';
 
 interface AppProps {
   routes: PluginRoute[];
 }
 
 const App: React.FC<AppProps> = ({routes}) => {
-  const location = useLocation();
-  const apiEndpoint = useApiEndpoint();
-  const isClusterAvailable = useSystemAccess(SystemAccess.agent);
-
-  const {setClusterDetails} = useClusterDetailsPick('setClusterDetails');
-
   const update = useUpdate();
-
-  const {open: openEndpointModal} = useModal({
-    title: 'Testkube API endpoint',
-    width: 693,
-    content: <EndpointModal />,
-    dataTestCloseBtn: 'endpoint-modal-close-button',
-    dataTestModalRoot: 'endpoint-modal',
-  });
-
-  useEffect(() => {
-    // Do not fire the effect if new endpoint is just being set up.
-    if (location.pathname === '/apiEndpoint') {
-      return;
-    }
-
-    if (!apiEndpoint && !isApiEndpointLocked()) {
-      openEndpointModal();
-      return;
-    }
-
-    // Avoid loading API details when we know the cluster is not available.
-    if (!isClusterAvailable) {
-      return;
-    }
-
-    getApiDetails(apiEndpoint!)
-      .then(setClusterDetails)
-      .catch(() => {
-        // Handle race condition
-        if (getApiEndpoint() !== apiEndpoint) {
-          return;
-        }
-
-        // Display popup
-        notificationCall('failed', 'Could not receive data from the specified API endpoint');
-        if (!isApiEndpointLocked()) {
-          openEndpointModal();
-        }
-      });
-  }, [apiEndpoint, isClusterAvailable]);
-
   return (
     <Suspense fallback={<Loading />}>
       <BannerList items={useGeneralSlot('banners')} onClose={update} />
--- packages/web/src/plugins/cluster/plugin.tsx
+++ packages/web/src/plugins/cluster/plugin.tsx
@@ -1,6 +1,16 @@
+import React, {useEffect} from 'react';
+
 import {StoreProvider, createPlugin, external} from '@testkube/plugins';
 
+import {useModal} from '@modal/hooks';
+
+import {EndpointModal, notificationCall} from '@molecules';
+
+import type ClusterStatusPlugin from '@plugins/cluster-status/plugin';
 import type GeneralPlugin from '@plugins/general/plugin';
+import type RouterPlugin from '@plugins/router/plugin';
+
+import {getApiDetails, getApiEndpoint, isApiEndpointLocked} from '@services/apiEndpoint';
 
 import {
   initializeClusterDetailsStore,
@@ -11,12 +21,62 @@ import {
 } from '@store/clusterDetails';
 
 const generalStub = external<typeof GeneralPlugin>();
+const routerStub = external<typeof RouterPlugin>();
+const clusterStatusStub = external<typeof ClusterStatusPlugin>();
 
-// TODO: Add provider for loading the Cluster Details automatically
 export default createPlugin('oss/cluster')
+  .needs(routerStub.data('location'))
   .needs(generalStub.data('useApiEndpoint'))
+  .needs(clusterStatusStub.data('isClusterAvailable'))
 
   .provider(tk => <StoreProvider store={initializeClusterDetailsStore} dependencies={[tk.data.useApiEndpoint()]} />)
   .data({useClusterDetails, useClusterDetailsPick, useClusterDetailsField, useClusterDetailsSync})
 
-  .init();
+  .init(tk => {
+    // TODO: Consider provider
+    tk.sync(() => {
+      const {useApiEndpoint, location, isClusterAvailable} = tk.data;
+      const apiEndpoint = useApiEndpoint();
+      const {setClusterDetails} = useClusterDetailsPick('setClusterDetails');
+
+      const {open: openEndpointModal} = useModal({
+        title: 'Testkube API endpoint',
+        width: 693,
+        content: <EndpointModal />,
+        dataTestCloseBtn: 'endpoint-modal-close-button',
+        dataTestModalRoot: 'endpoint-modal',
+      });
+
+      useEffect(() => {
+        // Do not fire the effect if new endpoint is just being set up.
+        if (location.pathname === '/apiEndpoint') {
+          return;
+        }
+
+        if (!apiEndpoint && !isApiEndpointLocked()) {
+          openEndpointModal();
+          return;
+        }
+
+        // Avoid loading API details when we know the cluster is not available.
+        if (!isClusterAvailable) {
+          return;
+        }
+
+        getApiDetails(apiEndpoint!)
+          .then(setClusterDetails)
+          .catch(() => {
+            // Handle race condition
+            if (getApiEndpoint() !== apiEndpoint) {
+              return;
+            }
+
+            // Display popup
+            notificationCall('failed', 'Could not receive data from the specified API endpoint');
+            if (!isApiEndpointLocked()) {
+              openEndpointModal();
+            }
+          });
+      }, [apiEndpoint, isClusterAvailable]);
+    });
+  });
