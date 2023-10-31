457f41d257dcf814beb0a8c032548a7ba8e25460
Dawid Rusnak <dawid@drcode.pl>
2023-10-31 16:19:11 +0100

    fix: isolate properly Router plugin

--- packages/web/src/plugins/router/plugin.tsx
+++ packages/web/src/plugins/router/plugin.tsx
@@ -11,10 +11,9 @@ import {ReactRouterLayer} from './components/ReactRouterLayer';
 export default createPlugin('oss/router')
   .order(-10)
 
-  // TODO: Pass base url from outside (Cloud)
   .define(config<string>()('baseUrl'))
   .define(data<string>()('baseUrl'))
-  .provider(tk => <ReactRouterLayer basename={tk.data.baseUrl} />)
+  .provider(<ReactRouterLayer />)
 
   // Scroll to top on a navigation to a new page
   .provider(tk => {
@@ -29,11 +28,13 @@ export default createPlugin('oss/router')
   .define(data<NavigateFunction>()('navigate'))
   .define(data<Record<string, string | undefined>>()('params'))
 
+  .provider(tk => {
+    tk.data.navigate = useLastCallback(useNavigate());
+    tk.data.location = useLocation();
+    tk.data.params = useParams();
+    return {type: Fragment, props: {}};
+  })
+
   .init((tk, cfg) => {
     tk.data.baseUrl = cfg.baseUrl;
-    tk.sync(() => {
-      tk.data.navigate = useLastCallback(useNavigate());
-      tk.data.location = useLocation();
-      tk.data.params = useParams();
-    });
   });
