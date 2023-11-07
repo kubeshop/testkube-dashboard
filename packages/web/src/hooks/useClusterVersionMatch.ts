376c32a2ed5c62fc27bf7812f8c2d1cbd6de02e8
Dawid Rusnak <dawid@drcode.pl>
2023-11-07 09:34:55 +0100

    fix: ignore commit hash for the cluster version check

--- packages/web/src/hooks/useClusterVersionMatch.ts
+++ packages/web/src/hooks/useClusterVersionMatch.ts
@@ -5,7 +5,8 @@ import {useClusterPlugin} from '@plugins/cluster/hooks';
 const useClusterVersionMatch = <T>(match: string, defaults?: T): boolean | T => {
   const useClusterDetails = useClusterPlugin.select(x => x.useClusterDetails);
   const clusterVersion = useClusterDetails(x => x.version);
-  return clusterVersion == null ? (defaults as T) : matchesVersion(clusterVersion, match);
+  const isHash = !/^v?[0-9]+\./.test(clusterVersion || '');
+  return clusterVersion == null || isHash ? (defaults as T) : matchesVersion(clusterVersion, match);
 };
 
 export default useClusterVersionMatch;
