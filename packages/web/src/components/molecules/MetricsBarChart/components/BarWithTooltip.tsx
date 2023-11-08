a59ab0504dd88e7bc17ef431acb90663682e41c3
Dawid Rusnak <dawid@drcode.pl>
2023-11-08 09:06:06 +0100

    feat: use shared openByName callback for bar clicking

--- packages/web/src/components/molecules/MetricsBarChart/components/BarWithTooltip.tsx
+++ packages/web/src/components/molecules/MetricsBarChart/components/BarWithTooltip.tsx
@@ -1,8 +1,5 @@
 import {memo} from 'react';
 
-import {useLastCallback} from '@hooks/useLastCallback';
-
-import {useEntityDetailsPick} from '@store/entityDetails';
 import {useExecutionDetailsPick} from '@store/executionDetails';
 
 import {SecondaryStatusColors, StatusColors} from '@styles/Colors';
@@ -23,15 +20,8 @@ export type BarConfig = {
 };
 
 const BarWithTooltip: React.FC<BarConfig> = props => {
-  const {executions} = useEntityDetailsPick('executions');
-  const {open} = useExecutionDetailsPick('open');
-  const onSelect = useLastCallback((name: string) => {
-    const targetRecord = executions.results?.find((item: any) => item.name === name);
-    if (targetRecord) {
-      open(targetRecord.id);
-    }
-  });
-  return <BarWithTooltipPure {...props} onSelect={onSelect} />;
+  const {openByName} = useExecutionDetailsPick('openByName');
+  return <BarWithTooltipPure {...props} onSelect={openByName} />;
 };
 
 export default memo(BarWithTooltip);
