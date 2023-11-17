4c0900870578603d47ec71efc454948bc892f18e
Dawid Rusnak <dawid@drcode.pl>
2023-11-17 10:12:09 +0100

    fix: escaped arguments while prettifying

--- packages/web/src/utils/prettifyArguments.ts
+++ packages/web/src/utils/prettifyArguments.ts
@@ -1,5 +1,4 @@
 export const prettifyArguments = (args: string) =>
-  args
-    .replace(/(".*?")|('.*?')|\s/g, (_: string, str1: string, str2: string) => str1 || str2 || '\n')
-    .replace(/\n{2,}/g, '\n')
-    .trim();
+  Array.from(args.match(/(\\.|("(\\.|[^"])*")|('(\\.|[^'])*')|\S)+/g) || [])
+    .map(x => x.trim())
+    .join('\n');
