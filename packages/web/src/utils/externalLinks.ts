dac7577896fa694fdfb02099f9d094bf4d751ac0
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2023-11-21 16:47:53 +0200

    chore: add docs external link

--- packages/web/src/utils/externalLinks.ts
+++ packages/web/src/utils/externalLinks.ts
@@ -30,6 +30,7 @@ export enum externalLinks {
   environmentMembers = 'https://docs.testkube.io/testkube-cloud/articles/environment-management#managing-environment-member-roles',
   notificationsAndWebhooks = 'https://docs.testkube.io/articles/webhooks',
   statusPages = 'https://docs.testkube.io/testkube-cloud/articles/status-pages',
+  githubActions = 'https://docs.testkube.io/articles/github-actions',
 }
 
 const crdCdn = `https://raw.githubusercontent.com/kubeshop/testkube-operator/${encodeURIComponent(
