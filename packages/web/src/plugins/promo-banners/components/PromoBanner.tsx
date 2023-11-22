d552525636a38f04c68e79f1182eacc08452ce8a
Dawid Rusnak <dawid@drcode.pl>
2023-11-22 14:26:40 +0100

    feat: add telemetry events and IDs for the promo banners

--- packages/web/src/plugins/promo-banners/components/PromoBanner.tsx
+++ packages/web/src/plugins/promo-banners/components/PromoBanner.tsx
@@ -7,6 +7,8 @@ import {MessagePanel} from '@molecules';
 
 import {usePromoBannersPlugin} from '@plugins/promo-banners/hooks';
 
+import {useTelemetry} from '@telemetry/hooks';
+
 import {PromoBannerPosition} from '../config';
 import {useDisplayedPromoBanner} from '../hooks/useDisplayedPromoBanner';
 import {applyUrlParams} from '../utils/applyUrlParams';
@@ -18,11 +20,17 @@ export interface PromoBannerProps {
 }
 
 export const PromoBanner: FC<PromoBannerProps> = ({position, style, className}) => {
+  const telemetry = useTelemetry();
   const navigate = useNavigate();
   const params = useParams();
   const {bannersClose} = usePromoBannersPlugin.pick('bannersClose');
   const close = useLastCallback(() => bannersClose(banner!.id));
+  const trackAndClose = useLastCallback(() => {
+    telemetry.event('promoBannerCloseClick', {id: banner?.id});
+    close();
+  });
   const banner = useDisplayedPromoBanner(position);
+  const id = banner?.id ? banner.id.replace(/[^a-zA-Z0-9-_]/g, '-') : null;
 
   const buttons = useMemo(
     () => [
@@ -31,12 +39,14 @@ export const PromoBanner: FC<PromoBannerProps> = ({position, style, className}) 
             {
               type: 'secondary' as const,
               text: banner!.secondaryLink.label,
+              id: `promo-banner-secondary_${id}`,
               isLink: true,
               linkConfig: {
                 href: applyUrlParams(banner!.secondaryLink.url, params),
                 target: banner!.secondaryLink!.target,
               },
               onClick: (event: any) => {
+                telemetry.event('promoBannerSecondaryClick', {id: banner.id});
                 if (banner!.secondaryLink!.url.startsWith('/') && banner!.secondaryLink!.target === '_top') {
                   event.preventDefault();
                   navigate(applyUrlParams(banner!.secondaryLink!.url, params));
@@ -50,9 +60,11 @@ export const PromoBanner: FC<PromoBannerProps> = ({position, style, className}) 
             {
               type: 'primary' as const,
               text: banner!.primaryLink.label,
+              id: `promo-banner-primary_${id}`,
               isLink: true,
               linkConfig: {href: applyUrlParams(banner!.primaryLink.url, params), target: banner!.primaryLink!.target},
               onClick: (event: any) => {
+                telemetry.event('promoBannerPrimaryClick', {id: banner.id});
                 close();
                 if (banner!.primaryLink!.url.startsWith('/') && banner!.primaryLink!.target === '_top') {
                   event.preventDefault();
@@ -69,12 +81,13 @@ export const PromoBanner: FC<PromoBannerProps> = ({position, style, className}) 
   return banner ? (
     <div style={style} className={className}>
       <MessagePanel
+        id={`promo-banner-container_${id}`}
         key={banner.id}
         title={banner.title}
         description={banner.description}
         type={banner.type}
         buttons={buttons}
-        onClose={banner.permanent ? undefined : close}
+        onClose={banner.permanent ? undefined : trackAndClose}
       />
     </div>
   ) : null;
