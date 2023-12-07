af2044da54256b0b5e47ea41f3c45b5f43d45687
Razvan Topliceanu <47887589+topliceanurazvan@users.noreply.github.com>
2023-12-07 09:25:23 +0200

    feat: add entity list promo component with plugin slot (#972)

--- packages/web/src/components/pages/Tests/TestDetails/TestDetails.tsx
+++ packages/web/src/components/pages/Tests/TestDetails/TestDetails.tsx
@@ -1,7 +1,9 @@
-import React, {FC} from 'react';
+import React, {FC, cloneElement, isValidElement} from 'react';
 import {useParams} from 'react-router-dom';
 
 import {EntityDetailsLayer, ExecutionDetailsLayer} from '@organisms/EntityDetails';
+
+import {useTestsSlotFirst} from '@plugins/tests-and-test-suites/hooks';
 
 import {
   useGetTestExecutionByIdQuery,
@@ -18,6 +20,18 @@ interface TestDetailsProps {
 
 const TestDetails: FC<TestDetailsProps> = ({tab}) => {
   const {id, execId, settingsTab} = useParams();
+  const entityPromoComponent = useTestsSlotFirst('entityListPromoComponent');
+
+  if (entityPromoComponent) {
+    return (
+      <>
+        {isValidElement(entityPromoComponent)
+          ? cloneElement(entityPromoComponent, {list: 'tests'} as Partial<unknown>)
+          : null}
+      </>
+    );
+  }
+
   return (
     <EntityDetailsLayer
       entity="tests"
