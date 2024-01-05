e202f8ffba04bb17a4bddf69f115e7bd48215c83
Razvan Topliceanu <47887589+topliceanurazvan@users.noreply.github.com>
2024-01-05 14:07:06 +0200

    fix: escape arguments containing spaces (#982)
    * fix: escape arguments containing spaces
    
    * fix: format args
    
    * fix: check if arg starts and ends with quotes
    
    * fix: restore functionality for inline
    
    * refactor: changes
    
    * refactor: changes
    
    * refactor: unit tests + inline args
    
    * refactor: rename
    
    * refactor: escape quotes
    
    * fix: add single quote and backtick
    
    * chore: import directly from utils
    
    * fix: unit tests
    
    * refactor: changes
    
    * feat: add unit test
    
    * fix: github warning

---
+++ packages/web/src/utils/escapeArguments.spec.ts
@@ -0,0 +1,33 @@
+import {escapeArguments} from './escapeArguments';
+
+describe('utils', () => {
+  describe('escapeArguments', () => {
+    it('should handle empty array', () => {
+      expect(escapeArguments([])).toEqual([]);
+    });
+
+    it('should handle array with one argument', () => {
+      expect(escapeArguments(['abc'])).toEqual(['abc']);
+    });
+
+    it('should handle array with multiple arguments', () => {
+      expect(escapeArguments(['abc', 'def'])).toEqual(['abc', 'def']);
+    });
+
+    it('should handle array with multiple arguments with spaces', () => {
+      expect(escapeArguments(['ab c', 'de f'])).toEqual(['"ab c"', '"de f"']);
+    });
+
+    it('should handle array with multiple arguments with quotes', () => {
+      expect(escapeArguments(['ab"c', 'de"f'])).toEqual(['ab\\"c', 'de\\"f']);
+    });
+
+    it('should handle array with multiple arguments with spaces and quotes', () => {
+      expect(escapeArguments(['ab"c', 'de f'])).toEqual(['ab\\"c', '"de f"']);
+    });
+
+    it('should not escape single quotes', () => {
+      expect(escapeArguments(["abc' def"])).toEqual([`"abc' def"`]);
+    });
+  });
+});
