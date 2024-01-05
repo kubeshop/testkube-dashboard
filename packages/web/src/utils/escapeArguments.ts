ffa1fbd81b0be61d35b5899c49d35827fa71dc0e
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2024-01-05 13:26:49 +0200

    fix: add single quote and backtick

--- packages/web/src/utils/escapeArguments.ts
+++ packages/web/src/utils/escapeArguments.ts
@@ -1,6 +1,6 @@
 export const escapeArguments = (args: string[]) => {
   return args.map(arg => {
-    const escapedArg = arg.replace(/(")/g, '\\$1');
+    const escapedArg = arg.replace(/(["'`])/g, '\\$1');
 
     return /\s/.test(escapedArg) ? `"${escapedArg}"` : escapedArg;
   });
