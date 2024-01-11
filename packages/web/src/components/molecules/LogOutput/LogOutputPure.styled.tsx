032b636abc9b35a453b6efaa738324761fea05f8
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2024-01-11 13:36:20 +0200

    feat: add placeholder when first running test

---
+++ packages/web/src/components/molecules/LogOutput/LogOutputPure.styled.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const InitialisingLogContainer = styled.div`
+  margin: 16px 0px 0px 16px;
+  display: flex;
+  gap: 16px;
+  align-items: center;
+  font-size: 12px;
+`;
