3ce765a74b9f6f73dd5ee2be4b05bc87563a0037
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2024-01-12 16:50:25 +0200

    refactor: filters container styling

--- packages/web/src/components/organisms/ExecutionsTable/ExecutionsFilters/ExecutionsFilters.styled.tsx
+++ packages/web/src/components/organisms/ExecutionsTable/ExecutionsFilters/ExecutionsFilters.styled.tsx
@@ -2,29 +2,13 @@ import {Input} from 'antd';
 
 import styled from 'styled-components';
 
-import {maxDevice} from '@src/styles/MediaQueries';
-
 export const FiltersContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 16px;
+  display: grid;
+  grid-template-columns: repeat(2, minmax(120px, 248px));
+  grid-column-gap: 16px;
   align-items: center;
   width: 100%;
   margin: 8px 0px 24px 0px;
-
-  & > * {
-    flex: 1 1 auto;
-    min-width: 160px;
-    max-width: 248px;
-  }
-
-  @media ${maxDevice.tablet} {
-    width: 100%;
-
-    & > * {
-      width: 100%;
-    }
-  }
 `;
 
 export const SearchInput = styled(Input)`
