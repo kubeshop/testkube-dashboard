6eca2ab24a2967f5c7cb1f13e89854e1691688c6
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2024-01-31 12:04:07 +0200

    refactor: artifacts download service

--- packages/web/src/services/artifacts.ts
+++ packages/web/src/services/artifacts.ts
@@ -2,32 +2,7 @@ import {getApiEndpoint} from '@services/apiEndpoint';
 
 import {getRtkBaseUrl, getRtkIdToken} from '@utils/rtk';
 
-const downloadFromUrl = (url: string, fileName: string): void => {
-  const a = document.createElement('a');
-  a.href = url;
-  a.download = fileName;
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
-};
-
-const downloadPotentialFile = async (response: Response, fileName: string): Promise<void> => {
-  if (!response.status || response.status >= 300) {
-    throw new Error('Something went wrong');
-  }
-
-  if (response.headers.get('content-type') === 'application/json') {
-    const data = await response.json();
-    const fileUrl = data.data?.url || data.url;
-    downloadFromUrl(fileUrl, fileName);
-  } else {
-    const fileUrl = window.URL.createObjectURL(await response.blob());
-    downloadFromUrl(fileUrl, fileName);
-    window.URL.revokeObjectURL(fileUrl);
-  }
-};
-
-export const downloadArtifact = async (
+export const getApiResponse = async (
   fileName: string,
   executionId: string,
   testName?: string,
@@ -49,11 +24,62 @@ export const downloadArtifact = async (
     headers: idToken ? {authorization: `Bearer ${idToken}`} : {},
   });
 
-  // Download the file
-  await downloadPotentialFile(response, fileName);
+  return response;
 };
 
-export const downloadArtifactArchive = async (fileName: string, executionId: string) => {
+export const getResponsefileUrl = async (response: Response) => {
+  if (!response.status || response.status >= 300) {
+    throw new Error('Something went wrong');
+  }
+
+  const data = await response.json();
+  const fileUrl = data.data?.url || data.url;
+
+  return fileUrl;
+};
+
+export const fetchContentFromSignedURL = async (signedURL: string): Promise<Blob> => {
+  const response = await fetch(signedURL);
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch content from signed URL: ${response.status} ${response.statusText}`);
+  }
+
+  return response.blob();
+};
+
+const downloadFromBlobURL = async (signedURL: string, fileName: string): Promise<void> => {
+  try {
+    const content = await fetchContentFromSignedURL(signedURL);
+    const url = URL.createObjectURL(content);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  } catch (error: any) {
+    throw new Error(`Error on downloading the content: ${error.message}`);
+  }
+};
+
+const downloadPotentialFile = async (response: Response, fileName: string): Promise<void> => {
+  const fileUrl = await getResponsefileUrl(response);
+  downloadFromBlobURL(fileUrl, fileName);
+};
+
+export const downloadArtifact = async (
+  fileName: string,
+  executionId: string,
+  testName?: string,
+  testSuiteName?: string
+): Promise<void> => {
+  const response = await getApiResponse(fileName, executionId, testName, testSuiteName);
+  downloadPotentialFile(response, fileName);
+};
+
+export const downloadArtifactArchive = async (fileName: string, executionId: string): Promise<void> => {
   const url = `/executions/${executionId}/artifact-archive`;
   const finalUrl = `${getApiEndpoint()}${getRtkBaseUrl('environment')}${url}`;
   const idToken = await getRtkIdToken();
