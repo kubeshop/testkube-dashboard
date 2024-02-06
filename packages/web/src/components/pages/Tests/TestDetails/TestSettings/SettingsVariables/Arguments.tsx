0f039899c820c148b9de672f93cc6f7734671cb2
Bogdan Hanea <bogdan.hanea@yahoo.com>
2024-02-06 13:14:32 +0200

    feat: arguments mode

--- packages/web/src/components/pages/Tests/TestDetails/TestSettings/SettingsVariables/Arguments.tsx
+++ packages/web/src/components/pages/Tests/TestDetails/TestSettings/SettingsVariables/Arguments.tsx
@@ -1,13 +1,14 @@
 import React, {useEffect, useMemo} from 'react';
 
-import {Form, Input} from 'antd';
+import {Form, Input, Select} from 'antd';
 
 import {ExternalLink} from '@atoms';
 
-import {Button, FullWidthSpace, Text} from '@custom-antd';
+import {Button, FormItem, FormItemLabel, FullWidthSpace, Text} from '@custom-antd';
 
 import {useLastCallback} from '@hooks/useLastCallback';
 
+import {ExecutionArgsModeEnum} from '@models/execution';
 import {Test} from '@models/test';
 
 import {CopyCommand, notificationCall} from '@molecules';
@@ -31,6 +32,7 @@ import {ArgumentsWrapper} from './Arguments.styled';
 
 type ArgumentsFormValues = {
   args: string;
+  argsMode: ExecutionArgsModeEnum;
 };
 
 interface ArgumentsProps {
@@ -47,8 +49,10 @@ const Arguments: React.FC<ArgumentsProps> = ({readOnly}) => {
 
   const entityArgs = details.executionRequest?.args || [];
   const initialArgs = useMemo(() => escapeArguments(entityArgs)?.join('\n') || '', [entityArgs]);
+  const initialArgsMode = useMemo(() => details.executionRequest?.args_mode, [details.executionRequest?.args_mode]);
 
   const currentArgs = Form.useWatch('args', form) || '';
+  const argsMode = Form.useWatch('argsMode', form) || '';
 
   const prettifiedArgs = useMemo(() => prettifyArguments(currentArgs), [currentArgs]);
   const currentArgsInline = useMemo(() => prettifiedArgs.replace(/\n+/g, ' '), [prettifiedArgs]);
@@ -59,7 +63,7 @@ const Arguments: React.FC<ArgumentsProps> = ({readOnly}) => {
     const argVal = currentArgs?.trim().split('\n').filter(Boolean) || [];
 
     // Reset the form when there is no actual change
-    if (argVal.join('\n') === initialArgs) {
+    if (argVal.join('\n') === initialArgs && argsMode === initialArgsMode) {
       form.resetFields();
     }
 
@@ -68,6 +72,7 @@ const Arguments: React.FC<ArgumentsProps> = ({readOnly}) => {
       executionRequest: {
         ...details.executionRequest,
         args: argVal,
+        args_mode: argsMode,
       },
     };
 
@@ -98,15 +103,43 @@ const Arguments: React.FC<ArgumentsProps> = ({readOnly}) => {
       description="Define arguments which will be passed to the test executor."
       footer={footer}
       form={form}
-      initialValues={{args: initialArgs}}
       disabled={!mayEdit}
       readOnly={readOnly}
+      initialValues={{args: initialArgs, argsMode: initialArgsMode}}
       onConfirm={onSaveForm}
       onCancel={onCancel}
     >
       <ArgumentsWrapper>
-        <CopyCommand command={currentArgsInline} isBordered additionalPrefix="executor-binary" />
+        <FormItem
+          name="argsMode"
+          key="argsMode"
+          label={
+            <FormItemLabel
+              text="Mode"
+              tooltipMessage="Choose between appending arguments to the executor or overriding them"
+            />
+          }
+        >
+          <Select
+            options={[
+              {
+                label: 'Append',
+                value: 'append',
+              },
+              {
+                label: 'Override',
+                value: 'override',
+              },
+            ]}
+          />
+        </FormItem>
         <FullWidthSpace size={16} direction="vertical">
+          <CopyCommand
+            command={currentArgsInline}
+            isBordered
+            additionalPrefix={`executor-binary ${argsMode === 'append' ? '<executor-args>' : ''}`}
+          />
+
           <Text className="regular middle" color={Colors.slate400}>
             Arguments passed to the executor (concat and passed directly to the executor)
           </Text>
@@ -118,9 +151,9 @@ const Arguments: React.FC<ArgumentsProps> = ({readOnly}) => {
               }}
               className="args-textarea"
               placeholder={`--flag="value value"
---flag-123=value
-value
-`}
+              --flag-123=value
+              value
+              `}
             />
           </Form.Item>
           <Button onClick={onPrettify} $customType="secondary" disabled={isPrettified || readOnly}>
