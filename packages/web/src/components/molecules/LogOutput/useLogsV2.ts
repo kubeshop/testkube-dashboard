2cfe3633eab068a43dd0bc1365ff35413cd11324
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2024-03-29 12:16:43 +0200

    feat: show log timestamp

--- packages/web/src/components/molecules/LogOutput/useLogsV2.ts
+++ packages/web/src/components/molecules/LogOutput/useLogsV2.ts
@@ -19,6 +19,19 @@ export const useLogsV2 = (executionId?: string, isRunning?: boolean) => {
     {
       onMessage: e => {
         const logData = JSON.parse(e.data);
+        let content = logData.content || '';
+        try {
+          const formattedTime = new Intl.DateTimeFormat('default', {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false, // Use 24-hour format
+          }).format(new Date(logData.time));
+          content = `[${formattedTime}] ${content}`;
+        } catch {
+          // Ignore
+        }
+        logData.content = content;
         setLogs(prev => [...prev, logData]);
       },
       shouldReconnect: () => Boolean(isRunning),
