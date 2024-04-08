65a873bf0a7493691473a9e5c6a849d525d5fd3b
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2024-04-08 15:26:03 +0300

    refactor: use API_HOST

--- packages/web/scripts/security.sh
+++ packages/web/scripts/security.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+API_HOST=$(echo $REACT_APP_API_SERVER_ENDPOINT | sed -e 's|http://||g' -e 's|https://||g')
+
 tempFile=$(mktemp /etc/nginx/tempfile.XXXXXXXX)
 
 cat > "${tempFile}" <<EOF
@@ -7,7 +9,7 @@ cat > "${tempFile}" <<EOF
         add_header X-Frame-Options "SAMEORIGIN";
         add_header X-Content-Type-Options "nosniff";
         add_header Referrer-Policy "strict-origin-when-cross-origin";
-        add_header Content-Security-Policy "default-src 'self'; style-src 'self' 'unsafe-inline'; connect-src 'self' blob:;";
+        add_header Content-Security-Policy "default-src 'self'; style-src 'self' 'unsafe-inline'; connect-src 'self' http://${API_HOST} https://${API_HOST} ws://${API_HOST} wss://${API_HOST} blob:;";
 EOF
 
 if [ "${ENABLE_SECURITY_HEADERS}" = "true" ]; then
